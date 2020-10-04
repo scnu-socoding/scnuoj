86174962d5cd02ae1cb025dcfcb6da185c64eae2
Bobby Rong <rjl931189261@126.com>
2020-10-04 14:28:06 +0800

    dependency update
    Former-commit-id: aa1b4d33c8d3b92eef3483581c900871dcf20468

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 请前往 [SoCoding 论坛](https://socoding.cn/t/topic/172) 了解新版 SCNUOJ 内测信息。
 
 SCNUOJ (2020) 的诞生离不开 [JNOJ](https://github.com/shi-yang/jnoj)，在此特别鸣谢 JNOJ 开发组。
+
+请注意：本仓库舍弃了 `vendor` 目录的追踪，因此部署本 OJ 时，需要先执行 `composer install`。
--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=jnoj',
-    'username' => 'root',
-    'password' => '123456',
+    'dsn' => 'mysql:host=localhost;dbname=scnuoj',
+    'username' => 'socoding',
+    'password' => 'socoding',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
