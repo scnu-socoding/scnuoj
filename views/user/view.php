63f01be19d40a6fc7ead8f0962121fd6d6e98292
Bobby Rong <rjl931189261@126.com>
2020-10-05 20:47:18 +0800

    update graph style

--- views/user/view.php
+++ views/user/view.php
@@ -20,100 +20,41 @@ $this->registerJsFile("/js/flot/jquery.flot.browser.js", ['depends' => 'yii\web\
 $this->registerJsFile("/js/flot/jquery.flot.drawSeries.js", ['depends' => 'yii\web\JqueryAsset']);
 $this->registerJsFile("/js/flot/jquery.flot.time.js", ['depends' => 'yii\web\JqueryAsset']);
 $plotJS = <<<EOT
-var contests_json = {$contests};
-var data1 = new Array();
-var data2 = new Array();
-var min_score = 6000, max_score = 0;
-for (var i in contests_json) {
-    if (min_score > contests_json[i].total)
-        min_score = contests_json[i].total;
-    if (max_score < contests_json[i].total)
-        max_score = contests_json[i].total;
-    data1.push([
-        contests_json[i].start_time * 1000,
-        contests_json[i].total, //1
-        988, //2
-        contests_json[i].title,
-        contests_json[i].title,
-        contests_json[i].rating_change,  //5
-        contests_json[i].rank, //6
-        contests_json[i].url,
-        contests_json[i].level, //8
-        contests_json[i].level,
-        "1571360",
-        contests_json[i].title,
-    ]);
-    data2.push([
-        contests_json[i].start_time * 1000, contests_json[i].total
-    ]);
-}
-var datas = [
-    {label: "{$model->username}", data: data1},
-    {clickable: false, hoverable: false, color: "red", data: data2}
-];
 
-var markings = [
-    { color: '#a00', lineWidth: 1, yaxis: { from: 3000 } },
-    { color: '#f33', lineWidth: 1, yaxis: { from: 2600, to: 2999 } },
-    { color: '#f77', lineWidth: 1, yaxis: { from: 2400, to: 2599 } },
-    { color: '#ffcc88', lineWidth: 1, yaxis: { from: 2150, to: 2399 } },
-    { color: '#f8f', lineWidth: 1, yaxis: { from: 1900, to: 2149 } },
-    { color: '#aaf', lineWidth: 1, yaxis: { from: 1650, to: 1899 } },
-    { color: '#77ddbb', lineWidth: 1, yaxis: { from: 1400, to: 1649 } },
-    { color: '#7f7', lineWidth: 1, yaxis: { from: 1150, to: 1399 } },
-    { color: '#ccc', lineWidth: 1, yaxis: { from: 0, to: 1149 } },
-];
-
-var options = {
-    lines: { show: true },
-    points: { show: true },
-    xaxis: { mode: "time" },
-    yaxis: { min: min_score - 400, max: max_score + 600, ticks: [1150, 1400, 1650, 1900, 2150, 2300, 2400, 2600, 3000] },
-    grid: { hoverable: true, markings: markings }
-};
-
-var plot = $.plot($("#placeholder"), datas, options);
-
-function showTooltip(x, y, contents) {
-    $('<div id="tooltip">' + contents + '</div>').css( {
-        position: 'absolute',
-        display: 'none',
-        top: y - 20,
-        left: x + 10,
-        border: '1px solid #fdd',
-        padding: '2px',
-        'font-size' : '11px',
-        'background-color': '#fee',
-        opacity: 0.80
-    }).appendTo("body").fadeIn(200);
-}
-
-var ctx = plot.getCanvas().getContext("2d");
-
-var prev = -1;
-$("#placeholder").bind("plothover", function (event, pos, item) {
-    if (item) {
-        if (prev != item.dataIndex) {
-            $("#tooltip").remove();
-            var params = data1[item.dataIndex];
-            var total = params[1];
-            var change = params[5] > 0 ? "+" + params[5] : params[5];
-            var contestName = params[11];
-            var contestUrl = params[7];
-            var rank = params[6];
-            var title = params[8];
-            var html = "= " + total + " (" + change + "), " + title + "<br/>"
-                            + "Rank: " + rank + "<br/>"
-                            + "<a href='" + contestUrl + "'>" + contestName + "</a>";
-            showTooltip(item.pageX, item.pageY, html);
-            setTimeout(function () {
-                $("#tooltip").fadeOut(200);
-                prev = -1;
-            }, 4000);
-            prev = item.dataIndex;
-        }
+    var contests_json = {$contests};
+    var data1 = new Array();
+    var cnt = 1;
+    for (var i in contests_json) {
+        data1.push([
+            cnt, contests_json[i].total
+        ]);
+        cnt += 1;
     }
-});
+
+    var dataset = [
+        {
+            data: data1,
+            lines: { show: true }
+        }
+    ];
+
+    var options = {
+        series: { 
+            lines: { show: true }, 
+            points: { show: true },
+            color: "red"
+        },
+        xaxis: {
+            tickDecimals: 0
+        },yaxis: {
+            tickDecimals: 0
+        }
+            
+    };
+
+    $.plot($("#placeholder"), dataset, options);
+
+
 EOT;
 $this->registerJs($plotJS);
 
