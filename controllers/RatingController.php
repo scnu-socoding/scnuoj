7992b38e0cc508572a77fc2a46c7f26dea0a5d8d
Bobby Rong <rjl931189261@126.com>
2020-10-05 23:18:46 +0800

    use username in problem standings
    Former-commit-id: a32a6573c76234679c04126cea363d68748d76ce

--- controllers/RatingController.php
+++ controllers/RatingController.php
@@ -34,7 +34,7 @@ class RatingController extends BaseController
 
     public function actionProblem()
     {
-        $query = (new Query())->select('u.id, u.nickname, u.rating, s.solved')
+        $query = (new Query())->select('u.id, u.username, u.rating, s.solved')
             ->from('{{%user}} AS u')
             ->innerJoin('(SELECT COUNT(DISTINCT problem_id) AS solved, created_by FROM {{%solution}} WHERE result=4 AND status=1 GROUP BY created_by ORDER BY solved DESC) as s',
                 'u.id=s.created_by')
