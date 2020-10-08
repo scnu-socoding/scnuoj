cd080af48deaf611b79064b6c10bf3225897841a
Bobby Rong <rjl931189261@126.com>
2020-10-08 13:40:32 +0800

    add breadcrumbs in contest pages

--- views/contest/status.php
+++ views/contest/status.php
@@ -14,6 +14,8 @@ use app\models\Contest;
 
 $this->title = $model->title;
 $this->params['model'] = $model;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Contest'), 'url' => ['/contest/index']];
+$this->params['breadcrumbs'][] = $this->title;
 $problems = $model->problems;
 
 $nav = [];
