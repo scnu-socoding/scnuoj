c9126bf2c4cf2c84d7ad08031e89017fea106908
Bobby Rong <rjl931189261@126.com>
2020-10-16 09:07:23 +0800

    add FontAwesomeAsset
    Former-commit-id: 617d73ec9ba2e8707030ecb58ed904bd242aa19a

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -32,5 +32,6 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap4\BootstrapAsset',
+        'app\assets\FontAwesomeAsset',
     ];
 }
