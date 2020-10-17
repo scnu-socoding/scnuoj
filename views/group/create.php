52037138c05b0a6207dea6932943a4a094420e18
yhssdl <yhssdl@163.com>
2020-10-17 15:51:01 +0800

    增加创建小组的设置选项

--- views/group/create.php
+++ views/group/create.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\models\User;
 use yii\helpers\Html;
 
 use app\models\User;
@@ -17,13 +18,33 @@ if (Yii::$app->user->identity->role !== User::ROLE_ADMIN && Yii::$app->user->ide
 $this->title = Yii::t('app', 'Create Group');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Groups'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+
+$DefGp = false;
+if(Yii::$app->user->isGuest || Yii::$app->setting->get('isDefGroup') == 0)
+{
+    $DefGp = false; 
+}
+elseif ((Yii::$app->setting->get('isDefGroup')==2) && (Yii::$app->user->identity->role === User::ROLE_ADMIN) ) {
+    $DefGp = true; 
+}
+elseif(Yii::$app->setting->get('isDefGroup')==3 && (Yii::$app->user->identity->role === User::ROLE_ADMIN || Yii::$app->user->identity->role === User::ROLE_VIP)){
+    $DefGp = true; 
+}
+else{
+    $DefGp = false;  
+}
+
+
+
 ?>
 <div class="group-create">
-
+    <?php if ($DefGp): ?>    
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
-
+    <?php else: ?>
+       <h3> 没有创建小组的权限！</h3>
+    <?php endif; ?>   
 </div>
