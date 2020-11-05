ca77ce0054f1db7afa1febfbfd0f115eb57fe3e0
Bobby Rong <rjl931189261@126.com>
2020-11-05 11:08:17 +0800

    update description in rating

--- views/rating/index.php
+++ views/rating/index.php
@@ -68,7 +68,7 @@ $this->title = Yii::t('app', 'Rating');
         <div class="card text-dark">
             <div class="card-body">
                 <h3>关于积分</h3>
-                <p>在参加 AK 杯、蓝桥杯热身赛、天梯赛选拔赛等比赛之后，参赛者将根据排名得到一定积分。如果参加了比赛但没有通过任何题目，不会计算比赛积分。</p>
+                <p>在参加比赛之后，参赛者将根据排名得到一定积分。如果参加了比赛但没有通过任何题目，不会计算比赛积分。</p>
                 <p>积分计算采用 Elo Ranking 算法，起始分为 1149。该算法已被牛客竞赛等多个知名判题系统采用。想要了解该算法的更多信息，请查看 <a href="https://en.wikipedia.org/wiki/Elo_rating_system" target="_blank" class="text-dark"> Wikipedia 相关词条</a>。</p>
                 <p>积分一定程度上反映了参赛者的程序设计能力，可供各位同学找准自己的定位。</p>
             </div>
