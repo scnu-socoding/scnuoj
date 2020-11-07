b84731a63905b65ad93f1279921eae9062a39c51
Bobby Rong <rjl931189261@126.com>
2020-11-07 13:03:47 +0800

    small bug fix
    Former-commit-id: b32d872bd1675f270493bfe0f1a48107aaa2b005

--- views/contest/problem.php
+++ views/contest/problem.php
@@ -52,8 +52,8 @@ $loadingImgUrl = Yii::getAlias('@web/images/loading.gif');
         <div class="col-md-9 problem-view">
             <?php if ($this->beginCache('contest_problem_view' . $model->id . '_' . $problem['num'] . '_ '. $problem['id'])): ?>
                 <h1><?= Html::encode(chr(65 + $problem['num']) . '. ' . $problem['title']) ?></h1>
-                <div class="alert alert-info">比赛题目通过其它方式分发，敬请留意相关信息。</div>
                 <?php if ($problem['description'] == '' && $problem['input'] == '' && $problem['output']): ?>
+                    <div class="alert alert-info">比赛题目通过其它方式分发，敬请留意相关信息。</div>
                 <?php else: ?>
                     <h3><?= Yii::t('app', 'Description') ?></h3>
                     <div class="content-wrapper">
