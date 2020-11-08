0b5266c2fdc4c3088c9aa9615dc9c784b45b2e4a
Bobby Rong <rjl931189261@126.com>
2020-11-08 15:13:11 +0800

    display some messages when applying patches

--- scnuoj-patches/apply.sh
+++ scnuoj-patches/apply.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-patch -p0 vendor/cebe/markdown/inline/EmphStrongTrait.php fix_latex_parse.patch
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [[ -f ${DIR}/../vendor/cebe/markdown/inline/EmphStrongTrait.php ]] && [[ -f ${DIR}/fix_latex_parse.patch ]]
+then
+    echo "==> applying fix_latex_parse.patch..."
+    patch -p0 ${DIR}/../vendor/cebe/markdown/inline/EmphStrongTrait.php ${DIR}/fix_latex_parse.patch
+fi
+echo "==> exiting..."
