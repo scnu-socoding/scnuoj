18583a9338934a9399aa4656e0ef8843dfeab9b6
Bobby Rong <rjl931189261@126.com>
2020-11-11 11:00:03 +0800

    fix conflict in creating group
    Former-commit-id: 7caff4d897088c9362013d74cb10c6291697d722

--- views/group/create.php
+++ views/group/create.php
@@ -3,17 +3,12 @@
 use app\models\User;
 use yii\helpers\Html;
 
-use app\models\User;
 use yii\filters\AccessControl;
 use yii\web\NotFoundHttpException;
 use yii\web\ForbiddenHttpException;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Group */
-
-if (Yii::$app->user->identity->role !== User::ROLE_ADMIN && Yii::$app->user->identity->role !== User::ROLE_VIP) {
-    throw new ForbiddenHttpException('You are not allowed to perform this action.');
-}
 
 $this->title = Yii::t('app', 'Create Group');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Groups'), 'url' => ['index']];
