45e779bbb231c83e711db2f849b03b8ec23a5c56
Bobby Rong <rjl931189261@126.com>
2020-11-12 09:33:19 +0800

    redesign discuss page
    Former-commit-id: 540e1fbdb8b618f57c914a28c59483b2b5598ba1

--- views/discuss/view.php
+++ views/discuss/view.php
@@ -18,53 +18,66 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="row">
     <div class="col">
         <div class="card bg-secondary text-white">
-        <div class="card-body">
-            <h3><?= Html::encode($this->title) ?></h3>
+            <div class="card-body">
+                <h3><?= Html::encode($this->title) ?></h3>
+            </div>
         </div>
-    </div>
-    <p></p>
-        <p>
-            <span class="glyphicon glyphicon-user"></span> <?= Html::a(Html::encode($model->user->nickname), ['/user/view', 'id' => $model->user->username]) ?>
-            &nbsp;•&nbsp;
-            <span class="glyphicon glyphicon-time"></span> <?= Yii::$app->formatter->asRelativeTime($model->created_at) ?>
-            <?php if (!Yii::$app->user->isGuest && (Yii::$app->user->id === $model->created_by || Yii::$app->user->identity->role == User::ROLE_ADMIN)): ?>
-                &nbsp;•&nbsp;
-                <span class="glyphicon glyphicon-edit"></span> <?= Html::a(Yii::t('app', 'Edit'), ['/discuss/update', 'id' => $model->id]) ?>
-                &nbsp;•&nbsp;
-                <span class="glyphicon glyphicon-trash"></span>
-                <?= Html::a(Yii::t('app', 'Delete'), ['/discuss/delete', 'id' => $model->id], [
-                    'data' => [
-                        'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                        'method' => 'post',
-                    ],
-                ]) ?>
-            <?php endif; ?>
-        </p>
-        <hr>
-        <?= Yii::$app->formatter->asMarkdown($model->content) ?>
-        <hr>
-        <p><?= Yii::t('app', 'Comments') ?>:</p>
-        <?php foreach ($replies as $reply): ?>
-            <div class="well">
-                <?= Yii::$app->formatter->asMarkdown($reply->content) ?>
-                <hr>
-                <span class="glyphicon glyphicon-user"></span> <?= Html::a(Html::encode($reply->user->nickname), ['/user/view', 'id' => $reply->user->id]) ?>
-                &nbsp;•&nbsp;
-                <span class="glyphicon glyphicon-time"></span> <?= Yii::$app->formatter->asRelativeTime($reply->created_at) ?>
+        <p></p>
+        <div class="card">
+            <div class="card-body">
+                <?= Yii::$app->formatter->asMarkdown($model->content) ?>
+            </div>
+            <div class="card-footer">
 
-                <?php if (!Yii::$app->user->isGuest && (Yii::$app->user->id === $reply->created_by || Yii::$app->user->identity->role == User::ROLE_ADMIN)): ?>
-                    &nbsp;•&nbsp;
-                    <span class="glyphicon glyphicon-edit"></span> <?= Html::a(Yii::t('app', 'Edit'), ['/discuss/update', 'id' => $reply->id]) ?>
-                    &nbsp;•&nbsp;
-                    <span class="glyphicon glyphicon-trash"></span>
-                    <?= Html::a(Yii::t('app', 'Delete'), ['/discuss/delete', 'id' => $reply->id], [
+                <?= Html::a(Html::encode($model->user->nickname), ['/user/view', 'id' => $model->user->username], ['class' => 'text-dark']) ?>
+                /
+                <span class="text-dark"><?= Yii::$app->formatter->asRelativeTime($model->created_at) ?></span>
+                <?php if (!Yii::$app->user->isGuest && (Yii::$app->user->id === $model->created_by || Yii::$app->user->identity->role == User::ROLE_ADMIN)): ?>
+                <span class="float-right">
+                    <?= Html::a('<i class="fas fa-fw fa-pen"></i>', ['/discuss/update', 'id' => $model->id], ['class' => 'text-dark']) ?>
+                    <?= Html::a('<i class="fas fa-fw fa-trash"></i>', ['/discuss/delete', 'id' => $model->id], [
                         'data' => [
                             'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
                             'method' => 'post',
                         ],
+                        'class' => 'text-dark',
                     ]) ?>
+                    <?php endif; ?>
+                </span>
+            </div>
+        </div>
+        <p></p>
+        <div class="card">
+            <div class="card-body">
+                <h3 style="margin-bottom:0"><?= Yii::t('app', 'Comments') ?></h3>
+            </div>
+        </div>
+        <p></p>
+        <?php foreach ($replies as $reply): ?>
+        <div class="card">
+            <div class="card-body">
+                <?= Yii::$app->formatter->asMarkdown($reply->content) ?>
+            </div>
+            <div class="card-footer">
+                <?= Html::a(Html::encode($reply->user->nickname), ['/user/view', 'id' => $reply->user->id], ['class' => 'text-dark']) ?>
+                /
+                <?= Yii::$app->formatter->asRelativeTime($reply->created_at) ?>
+
+                <?php if (!Yii::$app->user->isGuest && (Yii::$app->user->id === $reply->created_by || Yii::$app->user->identity->role == User::ROLE_ADMIN)): ?>
+                <span class="float-right">
+                    <?= Html::a('<i class="fas fa-fw fa-pen"></i>', ['/discuss/update', 'id' => $reply->id], ['class' => 'text-dark']) ?>
+                    <?= Html::a('<i class="fas fa-fw fa-trash"></i>', ['/discuss/delete', 'id' => $reply->id], [
+                        'data' => [
+                            'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                            'method' => 'post',
+                        ],
+                        'class' => 'text-dark'
+                    ]) ?>
+                </span>
                 <?php endif; ?>
             </div>
+        </div>
+        <p></p>
         <?php endforeach; ?>
         <?= \yii\widgets\LinkPager::widget([
             'pagination' => $pages,
@@ -72,19 +85,19 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="well">
 
             <?php if (Yii::$app->user->isGuest): ?>
-                <?= app\widgets\login\Login::widget(); ?>
+            <?= app\widgets\login\Login::widget(); ?>
             <?php else: ?>
-                <?php $form = ActiveForm::begin(); ?>
+            <?php $form = ActiveForm::begin(); ?>
 
-                <?= $form->field($newDiscuss, 'content')->widget('app\widgets\editormd\Editormd')->label(false); ?>
+            <?= $form->field($newDiscuss, 'content')->widget('app\widgets\editormd\Editormd')->label(false); ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton(Yii::t('app', 'Reply'), ['class' => 'btn btn-primary']) ?>
-                </div>
-                <?php ActiveForm::end(); ?>
+            <div class="form-group">
+                <?= Html::submitButton(Yii::t('app', 'Reply'), ['class' => 'btn btn-block btn-outline-secondary']) ?>
+            </div>
+            <?php ActiveForm::end(); ?>
             <?php endif; ?>
 
-           
+
         </div>
     </div>
 </div>
