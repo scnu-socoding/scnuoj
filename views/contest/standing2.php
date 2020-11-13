13d2730249fd66e887ec5490a77b0378b89f2323
Bobby Rong <rjl931189261@126.com>
2020-11-13 10:34:53 +0800

    redesign public standing board

--- views/contest/standing2.php
+++ views/contest/standing2.php
@@ -1,38 +1,37 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
 
 /* @var $model app\models\Contest */
 
 $this->title = $model->title;
 ?>
 
-<div class="wrap">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3 text-left">
-                <strong>Start </strong>
-                <?= $model->start_time ?>
-            </div>
-            <div class="col-md-6 text-center">
-                <h2 class="contest-title"><?= Html::a(Html::encode($model->title), ['/contest/view', 'id' => $model->id]) ?></h2>
-            </div>
-            <div class="col-md-3 text-right">
-                <strong>End </strong>
-                <?= $model->end_time ?>
-            </div>
-        </div>
-        <?php echo $this->render('standing', [
-            'model' => $model,
-            'rankResult' => $rankResult,
-            'showStandingBeforeEnd' => $showStandingBeforeEnd
-        ]); ?> 
-    </div>
-</div>
 
-<footer class="footer">
-    <div class="container">
-    <p class="pull-left"><center>&copy; SCNU SoCoding <?= date('Y') ?></center></p>
-        <!-- <p class="pull-left">&copy; <?= Yii::$app->setting->get('ojName') ?> OJ <?= date('Y') ?></p> -->
-    </div>
-</footer>
+<?php
+    $menuItems = [
+        [
+            'label' => '<span class="glyphicon glyphicon-home"></span> ' . Yii::t('app', 'Information'),
+            'url' => ['contest/view', 'id' => $model->id],
+            'linkOptions' => ['class' => 'text-dark']
+        ],
+        [
+            'label' => '<span class="glyphicon glyphicon-glass"></span> ' . Yii::t('app', 'Standing'),
+            'url' => Url::toRoute(['/contest/standing2', 'id' => $model->id]),
+            'linkOptions' => ['class' => 'text-dark active'],
+            // 'visible' => $model->getRunStatus() != Contest::STATUS_NOT_START
+        ]
+    ];
+    echo Nav::widget([
+        'items' => $menuItems,
+        'options' => ['class' => 'nav nav-tabs hidden-print', 'style' => 'margin-bottom: 15px'],
+         'encodeLabels' => false
+]) ?>
+<?php echo $this->render('standing', [
+    'model' => $model,
+    'rankResult' => $rankResult,
+    'showStandingBeforeEnd' => $showStandingBeforeEnd
+]); ?>
--- views/layouts/contest.php
+++ views/layouts/contest.php
@@ -29,7 +29,9 @@ $status = $model->getRunStatus();
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?><?php if(Html::encode($this->title) != Yii::$app->setting->get('ojName')) echo " - " . Yii::$app->setting->get('ojName');?></title>
+    <title>
+        <?= Html::encode($this->title) ?><?php if(Html::encode($this->title) != Yii::$app->setting->get('ojName')) echo " - " . Yii::$app->setting->get('ojName');?>
+    </title>
     <?php $this->head() ?>
     <link rel="shortcut icon" href="<?= Yii::getAlias('@web') ?>/favicon.ico">
     <style>
@@ -40,6 +42,13 @@ $status = $model->getRunStatus();
 </head>
 
 <body style="padding-top: 56px;">
+
+    <div class="progress hidden-print rounded-0 bg-light">
+        <div class="progress-bar progress-bar-success" id="contest-progress" role="progressbar" aria-valuenow="60"
+            aria-valuemin="0" aria-valuemax="100" style="width: 1%;">
+        </div>
+    </div>
+
     <?php $this->beginBody() ?>
 
     <div>
@@ -128,11 +137,7 @@ $status = $model->getRunStatus();
     NavBar::end();
     ?>
 
-        <div class="progress hidden-print rounded-0 bg-light">
-            <div class="progress-bar progress-bar-success" id="contest-progress" role="progressbar" aria-valuenow="60"
-                aria-valuemin="0" aria-valuemax="100" style="width: 1%;">
-            </div>
-        </div>
+
 
         <div class="container-fluid">
             <div class="col-lg-10 offset-lg-1">
@@ -141,49 +146,24 @@ $status = $model->getRunStatus();
             'itemTemplate' => "<li class=\"breadcrumb-item\">{link}</li>\n",
             'activeItemTemplate' => "<li class=\"breadcrumb-item active\">{link}</li>\n"
         ]) ?> -->
-                <?= Alert::widget() ?>
-                <!-- <div class="contest-info">
-                <div class="row">
-                    <div class="col-md-3 text-left hidden-print">
-                        <strong><?= Yii::t('app', 'Start') ?> </strong>
-                        <?= $model->start_time ?>
-                    </div>
-                    <div class="col-md-6 text-center">
-                        <h2 class="contest-title">
-                            <?= Html::encode($model->title) ?>
-                            <?php if ($model->group_id != 0 && $model->isContestAdmin()): ?>
-                            <small>
-                                <?= Html::a('<span class="glyphicon glyphicon-cog"></span> ' . Yii::t('app', 'Setting'),
-                                    ['/homework/update', 'id' => $model->id]) ?>
-                            </small>
-                            <?php endif; ?>
-                        </h2>
-                    </div>
-                    <div class="col-md-3 text-right hidden-print">
-                        <strong><?= Yii::t('app', 'End') ?> </strong>
-                        <?= $model->end_time ?>
-                    </div>
-                </div>
 
-                <div class="text-center hidden-print">
-                    <strong><?= Yii::t('app', 'Now') ?></strong>
-                    <span id="nowdate"> <?= date("Y-m-d H:i:s") ?></span>
-                </div>
-            </div> 
-            <hr> -->
 
                 <br />
-                <?php if ($status == $model::STATUS_NOT_START): ?>
-                <div class="contest-countdown text-center">
-                    <div id="countdown"></div>
+                <?= Alert::widget() ?>
+
+                <?php if (!$model->canView()): ?>
+                <?= $content ?>
+                <?php elseif ($status == $model::STATUS_NOT_START): ?>
+                <div class="card">
+                    <div class="card-body text-center">
+                        <h1 id="countdown"></h1>
+                    </div>
                 </div>
                 <?php if (!empty($model->description)): ?>
-                <div class="contest-desc">
+                <!-- <div class="contest-desc">
                     <?= Yii::$app->formatter->asMarkdown($model->description) ?>
-                </div>
+                </div> -->
                 <?php endif; ?>
-                <?php elseif (!$model->canView()): ?>
-                <?= $content ?>
                 <?php else: ?>
                 <div class="contest-view">
                     <?php
@@ -262,8 +242,9 @@ $status = $model->getRunStatus();
         m = x.getMinutes();
         s = x.getSeconds();
 
-        n = y + "-" + (mon >= 10 ? mon : "0" + mon) + "-" + (d >= 10 ? d : "0" + d) + " " + (h >= 10 ? h : "0" + h) + ":" + (m >= 10 ? m : "0" + m) + ":" + (s >= 10 ?
-            s : "0" + s);
+        n = y + "-" + (mon >= 10 ? mon : "0" + mon) + "-" + (d >= 10 ? d : "0" + d) + " " + (h >= 10 ? h : "0" + h) +
+            ":" + (m >= 10 ? m : "0" + m) + ":" + (s >= 10 ?
+                s : "0" + s);
         if (document.getElementById('nowdate')) {
             document.getElementById('nowdate').innerHTML = n;
         }
