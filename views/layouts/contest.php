14066a886fe7a8a78edd3b4af54c81eb5a5d9e81
Bobby Rong <rjl931189261@126.com>
2020-11-16 19:43:50 +0800

    change default value of progress bar in contest section

--- views/layouts/contest.php
+++ views/layouts/contest.php
@@ -45,7 +45,7 @@ $status = $model->getRunStatus();
 
     <div class="progress hidden-print rounded-0 bg-light">
         <div class="progress-bar progress-bar-success" id="contest-progress" role="progressbar" aria-valuenow="60"
-            aria-valuemin="0" aria-valuemax="100" style="width: 1%;">
+            aria-valuemin="0" aria-valuemax="100" style="width: 0%;">
         </div>
     </div>
 
