92228cc6176fa3e5b31da5f9b01ae088cc3e2737
Bobby Rong <rjl931189261@126.com>
2020-11-17 21:49:35 +0800

    redesign user view page
    Former-commit-id: f577b309ca94c1641b5bfb9adf3fd439df98a085

--- views/user/view.php
+++ views/user/view.php
@@ -20,56 +20,29 @@ $recentSubmission = $model->getRecentSubmission();
 <p></p>
 <?php if ($model->role != \app\models\User::ROLE_PLAYER): ?>
 <div class="row">
-    <div class="col-md-4 col-lg-3">
-        <?= DetailView::widget([
-                    'model' => $model,
-                    'attributes' => [
-                        'username',
-                        'nickname',
-                        [
-                            'attribute' => Yii::t('app', 'Major'),
-                            'value' => function ($model, $widget) {
-                                return Html::encode($model->profile->major);
-                            },
-                            'format' => 'raw'
-                        ],
-                        [
-                            'attribute' => Yii::t('app', 'Student Number'),
-                            'value' => function ($model, $widget) {
-                                return $model->profile->student_number;
-                            },
-                            'format' => 'raw'
-                        ],
-                        [
-                            'attribute' => '积分',
-                            'value' => function ($model, $widget) {
-                                return isset($model->rating)?$model->rating:'Unrated';
-                            },
-                            'format' => 'raw'
-                        ]
-                    ],
-                ]) ?>
-    </div>
+
     <div class="col-md-8 col-lg-9">
-        <?php if($model->profile->personal_intro != ''):?>
+
         <div class="card">
+            <div class="card-header">个人档案</div>
             <div class="card-body">
+                <?php if($model->profile->personal_intro != ''):?>
                 <?= Yii::$app->formatter->asMarkdown($model->profile->personal_intro) ?>
+                <?php else:?>
+                没有找到数据。
+                <?php endif;?>
             </div>
         </div>
         <p></p>
-        <?php endif;?>
-        <?php if(!empty($recentSubmission)):?>
-            <div class="card">
-                <div class="card-body">
-                    <h3 style="margin-bottom:0">最近提交</h3>
-                </div>
-            </div>
-            <p></p>
-            <div class="list-group">
+
+
+        <div class="card">
+            <div class="card-header">最近提交</div>
+            <div class="card-body">
+                <?php if(!empty($recentSubmission)):?>
                 <?php foreach ($recentSubmission as $submission): ?>
                 <a href="<?= \yii\helpers\Url::toRoute(['/solution/detail', 'id' => $submission['id']]) ?>"
-                    class="list-group-item list-group-item-action">
+                    class="list-group-item list-group-item-action border-left-0 border-right-0">
                     <span>
                         <?= Html::encode($submission['problem_id'] . '. '. $submission['title']) ?>
                     </span>
@@ -79,61 +52,99 @@ $recentSubmission = $model->getRecentSubmission();
                     </span>
                 </a>
                 <?php endforeach; ?>
+                <?php else:?>
+                没有找到数据。
+                <?php endif;?>
             </div>
-        <?php endif;?>
-    </div>
-</div>
-<hr>
-<h3>已解答 <small>(<?= count($solutionStats['solved_problem']) ?>)</small></h3>
-<!-- <ul> -->
-<?php foreach ($solutionStats['solved_problem'] as $p): ?>
-<?= Html::a($p, ['/problem/view', 'id' => $p], ['class' => 'badge badge-secondary']) ?>
-<?php endforeach; ?>
-<!-- </ul> -->
-<hr>
-<h3>未解答 <small>(<?= count($solutionStats['unsolved_problem']) ?>)</small></h3>
-<!-- <ul> -->
-<?php foreach ($solutionStats['unsolved_problem'] as $p): ?>
-<?= Html::a($p, ['/problem/view', 'id' => $p], ['class' => 'badge badge-secondary']) ?>
-<?php endforeach; ?>
-<!-- </ul> -->
+        </div>
+        <p></p>
 
-<hr>
-<h2>统计</h2>
-<div class="row">
-    <div class="left-list col-md-6">
-        <ul class="stat-list">
-            <li>
-                <strong>提交总数</strong><span> <?= $solutionStats['all_count'] ?></span>
-            </li>
-            <li>
-                <strong>通过</strong><span> <?= $solutionStats['ac_count'] ?></span>
-            </li>
-            <li>
-                <strong>通过率</strong>
-                <span>
-                    <?= $solutionStats['all_count'] == 0 ? 0 : number_format($solutionStats['ac_count'] / $solutionStats['all_count'] * 100, 2) ?>
-                    %
+        <div class="card">
+            <div class="card-header">已解答 <small class="text-secondary"><?= count($solutionStats['solved_problem']) ?>
+                    Problem<?php if(count($solutionStats['solved_problem'])!=1) echo"s"; ?></small></div>
+            <div class="card-body">
+                <?php if(count($solutionStats['solved_problem']) != 0):?>
+                <?php foreach ($solutionStats['solved_problem'] as $p): ?>
+                <?= Html::a('<code>' . $p . '</code>', ['/problem/view', 'id' => $p], ['class' => 'badge badge-light text-secondary']) ?>
+                <?php endforeach; ?>
+                <?php else:?>
+                没有找到数据。
+                <?php endif;?>
+            </div>
+        </div>
+        <p></p>
+
+
+        <div class="card">
+            <div class="card-header">未解答 <small class="text-secondary"><?= count($solutionStats['unsolved_problem']) ?>
+                    Problem<?php if(count($solutionStats['unsolved_problem'])!=1) echo"s"; ?></small></div>
+            <div class="card-body">
+                <?php if(count($solutionStats['unsolved_problem']) != 0):?>
+                <?php foreach ($solutionStats['unsolved_problem'] as $p): ?>
+                <?= Html::a('<code>' . $p . '</code>', ['/problem/view', 'id' => $p], ['class' => 'badge badge-light text-secondary']) ?>
+                <?php endforeach; ?>
+                <?php else:?>
+                没有找到数据。
+                <?php endif;?>
+            </div>
+        </div>
+
+    </div>
+    <div class="col-md-4 col-lg-3">
+
+        
+        <div class="list-group">
+            <div class="list-group-item">
+                <?= Yii::t('app', 'Username') ?><span
+                    class="float-right text-secondary"><?= Html::encode($model->username)?></span>
+            </div>
+            <div class="list-group-item">
+                <?= Yii::t('app', 'Nickname') ?><span
+                    class="float-right text-secondary"><?= Html::encode($model->nickname)?></span>
+            </div>
+            <div class="list-group-item">
+                <?= Yii::t('app', 'Major') ?><span
+                    class="float-right text-secondary"><?= Html::encode($model->profile->major)?></span>
+            </div>
+            <div class="list-group-item">
+                <?=  Yii::t('app', 'Student Number') ?><span
+                    class="float-right text-secondary"><?= $model->profile->student_number?></span>
+            </div>
+            <div class="list-group-item">
+                积分<span class="float-right text-secondary"><?= isset($model->rating)?$model->rating:'Unrated'?></span>
+            </div>
+        </div>
+        <p></p>
+        <div class="list-group">
+            <div class="list-group-item">
+                提交总数<span class="float-right text-secondary"> <?= $solutionStats['all_count'] ?></span>
+            </div>
+            <div class="list-group-item">
+                通过<span class="float-right text-secondary"> <?= $solutionStats['ac_count'] ?></span>
+            </div>
+            <div class="list-group-item">
+                通过率
+                <span class="float-right text-secondary">
+                    <?= $solutionStats['all_count'] == 0 ? 0 : number_format($solutionStats['ac_count'] / $solutionStats['all_count'] * 100, 2) ?>%
                 </span>
-            </li>
-        </ul>
-    </div>
-    <div class="right-list col-md-6">
-        <ul class="stat-list">
-            <li>
-                <strong>错误解答</strong><span> <?= $solutionStats['wa_count'] ?></span>
-            </li>
-            <li>
-                <strong>时间超限</strong><span> <?= $solutionStats['tle_count'] ?></span>
-            </li>
-            <li>
-                <strong>编译错误</strong><span> <?= $solutionStats['ce_count'] ?></span>
-            </li>
-        </ul>
+            </div>
+            <div class="list-group-item">
+                错误解答<span class="float-right text-secondary"> <?= $solutionStats['wa_count'] ?></span>
+            </div>
+            <div class="list-group-item">
+                时间超限<span class="float-right text-secondary"> <?= $solutionStats['tle_count'] ?></span>
+            </div>
+            <div class="list-group-item">
+                编译错误<span class="float-right text-secondary"> <?= $solutionStats['ce_count'] ?></span>
+            </div>
+        </div>
     </div>
 </div>
+
+
 <?php else: ?>
-<p>用户名：<?= Html::encode($model->username) ?></p>
-<p>线下赛参赛账户．</p>
-<?php endif; ?>
+<div class="card">
+    <div class="card-body">
+        线下赛参赛账户。 </div>
 </div>
+<?php endif; ?>
