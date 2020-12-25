0abfd227c2ccdd8df833fece24ed749b2f59c961
深蓝 <yhssdl@163.com>
2020-12-25 08:53:47 +0800

    修正进后台问题的验题界面的问题
    Former-commit-id: 252fb7de714d653a96a4bde2c15c54dd8814e295

--- modules/admin/views/problem/verify.php
+++ modules/admin/views/problem/verify.php
@@ -13,7 +13,7 @@ $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Problems'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 $this->params['model'] = $model;
-
+$loadingImgUrl = Yii::getAlias('@web/images/loading.gif');
 ?>
 <div class="solutions-view">
     <h1>
@@ -23,40 +23,62 @@ $this->params['model'] = $model;
     <div class="table-responsive">
         <table class="table table-bordered table-rank">
             <thead>
-            <tr>
-                <th width="60px">Run ID</th>
-                <th width="60px">Submited Time</th>
-                <th width="100px">Result</th>
-                <th width="60px">Language</th>
-                <th width="70px">Time</th>
-                <th width="80px">Memory</th>
-                <th width="80px">Code Length</th>
-            </tr>
+                <tr>
+                    <th width="60px">Run ID</th>
+                    <th width="60px">Submited Time</th>
+                    <th width="100px">Result</th>
+                    <th width="60px">Language</th>
+                    <th width="70px">Time</th>
+                    <th width="80px">Memory</th>
+                    <th width="80px">Code Length</th>
+                </tr>
             </thead>
             <tbody>
-            <?php foreach ($solutions as $solution): ?>
-                <tr>
-                    <th><?= $solution['id'] ?></th>
-                    <th>
-                        <?= $solution['created_at'] ?>
-                    </th>
-                    <th>
-                        <?= Html::a(Solution::getResultList($solution['result']), ['/solution/detail', 'id' => $solution['id']], ['target' => '_blank']); ?>
-                    </th>
-                    <th>
-                        <?= Html::a(Solution::getLanguageList($solution['language']), ['/solution/detail', 'id' => $solution['id']], ['target' => '_blank']) ?>
-                    </th>
-                    <th>
-                        <?= $solution['time'] ?>
-                    </th>
-                    <th>
-                        <?= $solution['memory'] ?>
-                    </th>
-                    <th>
-                        <?= $solution['code_length'] ?>
-                    </th>
-                </tr>
-            <?php endforeach; ?>
+                <?php foreach ($solutions as $solution) : ?>
+                    <tr>
+                        <th><?= $solution['id'] ?></th>
+                        <th><?= $solution['created_at'] ?></th>
+                        <th>
+                            <?php
+                            if ($solution['result'] <= Solution::OJ_WAITING_STATUS) {
+                                $waitingHtmlDom = 'waiting="true"';
+                                $loadingImg = "<img src=\"{$loadingImgUrl}\">";
+                            } else {
+                                $waitingHtmlDom = 'waiting="false"';
+                                $loadingImg = "";
+                            }
+                            $innerHtml =  'data-verdict="' . $solution['result'] . '" data-submissionid="' . $solution['id'] . '" ' . $waitingHtmlDom;
+                            if ($solution['result'] == Solution::OJ_AC) {
+                                $span = '<strong class="text-success"' . $innerHtml . '>' . Solution::getResultList($solution['result']) . '</strong>';
+                                echo Html::a(
+                                    $span,
+                                    ['/solution/source', 'id' => $solution['id']],
+                                    ['onclick' => 'return false', 'data-click' => "solution_info", 'data-pjax' => 0]
+                                );
+                            } else {
+                                $span = '<strong class="text-danger" ' . $innerHtml . '>' . Solution::getResultList($solution['result']) . $loadingImg . '</strong>';
+                                echo Html::a(
+                                    $span,
+                                    ['/solution/result', 'id' => $solution['id']],
+                                    ['onclick' => 'return false', 'data-click' => "solution_info", 'data-pjax' => 0]
+                                );
+                            }
+                            ?>
+                        </th>
+                        <th>
+                            <?= Html::a(Solution::getLanguageList($solution['language']), ['/solution/detail', 'id' => $solution['id']], ['target' => '_blank']) ?>
+                        </th>
+                        <th>
+                            <?= $solution['time'] ?>
+                        </th>
+                        <th>
+                            <?= $solution['memory'] ?>
+                        </th>
+                        <th>
+                            <?= $solution['code_length'] ?>
+                        </th>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </div>
