22682d9008300ea2e40c8a1ebbbe05005a480430
Bobby Rong <rjl931189261@126.com>
2020-12-28 18:11:50 +0800

    move problem list to a separate page
    Former-commit-id: 1e9b9061bb7cb86e2b9b2da30613a0af4059e17c

---
+++ views/contest/problem_index.php
@@ -0,0 +1,52 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use app\models\Contest;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Contest */
+/* @var $solution app\models\Solution */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $data array */
+
+$this->title = $model->title;
+$this->params['model'] = $model;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Contest'), 'url' => ['/contest/index']];
+$this->params['breadcrumbs'][] = $this->title;
+
+$problems = $model->problems;
+$loginUserProblemSolvingStatus = $model->getLoginUserProblemSolvingStatus();
+$submissionStatistics = $model->getSubmissionStatistics();
+?>
+
+
+
+<div class="row">
+    <div class="col">
+        <div class="list-group">
+            <?php if ($model->type == Contest::TYPE_OI && $model->getRunStatus() == Contest::STATUS_RUNNING): ?>
+            <?php foreach ($problems as $key => $p): ?>
+            <?php if (!isset($loginUserProblemSolvingStatus[$p['problem_id']])): ?>
+            <?= Html::a(chr(65 + $key) . '. ' . $p['title'] . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'list-group-item list-group-item-action']) ?>
+            <?php else: ?>
+            <?= Html::a(chr(65 + $key) . '. ' . $p['title'] . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'list-group-item list-group-item-action list-group-item-warning']) ?>
+            <?php endif; ?>
+            <?php endforeach; ?>
+            <?php else: ?>
+            <?php foreach ($problems as $key => $p): ?>
+            <?php if (!isset($loginUserProblemSolvingStatus[$p['problem_id']])): ?>
+            <?= Html::a(chr(65 + $key) . '. ' . $p['title'] . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['solved'] . ' 通过 / ' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'list-group-item list-group-item-action']) ?>
+            <?php elseif ($loginUserProblemSolvingStatus[$p['problem_id']] == \app\models\Solution::OJ_AC): ?>
+            <?= Html::a(chr(65 + $key) . '. ' . $p['title'] . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['solved'] . ' 通过 / ' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'list-group-item list-group-item-action list-group-item-success']) ?>
+            <?php elseif ($loginUserProblemSolvingStatus[$p['problem_id']] < 4): ?>
+            <?= Html::a(chr(65 + $key) . '. ' . $p['title'] . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['solved'] . ' 通过 / ' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'list-group-item list-group-item-action list-group-item-warning']) ?>
+            <?php else: ?>
+            <?= Html::a(chr(65 + $key) . '. ' . $p['title'] . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['solved'] . ' 通过 / ' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'list-group-item list-group-item-action list-group-item-danger']) ?>
+            <?php endif; ?>
+            <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
+        <p></p>
+    </div>
+</div>
