de80e19441c9aa33212e6d386a0417706a45b7a6
Bobby Rong <rjl931189261@126.com>
2020-12-29 19:50:50 +0800

    update layout when reject entering contest
    Former-commit-id: 027726c4a540a0c156f57e6d920744ce24b19ef6

--- views/contest/forbidden.php
+++ views/contest/forbidden.php
@@ -15,76 +15,99 @@ $this->title = $model->title;
 $this->params['model'] = $model;
 ?>
 <?php if ($model->status == Contest::STATUS_PRIVATE): ?>
-    <div class="card bg-secondary text-white">
-        <div class="card-body">
-            <h3>私密比赛</h3>
-        </div>
+<div class="card bg-secondary text-white">
+    <div class="card-body">
+        <h3>私密比赛</h3>
     </div>
-    <p></p>
-    <div class="card">
-        <div class="card-body">
-            该比赛仅参赛人员可见。
-        </div>
+</div>
+<p></p>
+<div class="card">
+    <div class="card-body">
+        该比赛仅参赛人员可见。
     </div>
-    <?php
-            $this->title = '私密比赛';
-            $this->params['model']->title = '';
-            $this->params['model']->start_time = '';
-            $this->params['model']->end_time = '';
-        ?>
+</div>
+<?php
+    $this->title = '私密比赛';
+    $this->params['model']->title = '';
+    $this->params['model']->start_time = '';
+    $this->params['model']->end_time = '';
+?>
 <?php else: ?>
-
-    <?php
-                $menuItems = [
-                    [
-                        'label' => '<span class="glyphicon glyphicon-home"></span> ' . Yii::t('app', 'Information'),
-                        'url' => ['contest/view', 'id' => $model->id],
-                        'linkOptions' => ['class' => 'text-dark active']
-                    ],
-                    [
-                        'label' => '<span class="glyphicon glyphicon-glass"></span> ' . Yii::t('app', 'Standing'),
-                        'url' => Url::toRoute(['/contest/standing2', 'id' => $model->id]),
-                        'linkOptions' => ['class' => 'text-dark'],
-                        'visible' => $model->getRunStatus() != Contest::STATUS_NOT_START
-                    ]
-                ];
-                echo Nav::widget([
-                    'items' => $menuItems,
-                    'options' => ['class' => 'nav nav-tabs hidden-print', 'style' => 'margin-bottom: 15px'],
-                    'encodeLabels' => false
-                ]) ?> 
-
-    <div class="card bg-secondary text-white">
-        <div class="card-body">
-            <h3><?= $this->title ?></h3>
+<?php
+    $menuItems = [
+        [
+            'label' => Yii::t('app', 'Information'),
+            'url' => ['contest/view', 'id' => $model->id],
+            'linkOptions' => ['class' => 'active']
+        ],
+        [
+            'label' => Yii::t('app', 'Standing'),
+            'url' => Url::toRoute(['/contest/standing2', 'id' => $model->id]),
+            'visible' => $model->getRunStatus() != Contest::STATUS_NOT_START
+        ]
+    ];
+    echo Nav::widget([
+        'items' => $menuItems,
+        'options' => ['class' => 'nav nav-pills hidden-print'],
+        'encodeLabels' => false
+    ])
+?>
+<p></p>
+<div class="row">
+    <div class="col-lg-8">
+        <div class="alert alert-light">
+            <i class="fas fa-fw fa-info-circle"></i>
+            <?php if (strtotime($model->end_time) >= 253370736000): ?>
+            <b>永久开放的题目集</b> 任何时候均可进行作答。
+            <?php else: ?>
+            <b>限时开放的题目集</b> 只有在规定时间内的作答才会被计入比赛正式榜单。
+            <?php endif; ?>
         </div>
-    </div>
-    <p></p>
-    <div class="card">
-        <div class="card-body">
-            您尚未报名参加该比赛，请先参赛，或比赛结束后再来访问。
-        </div>
-    </div>
-    <p></p>
-    <?php if ($model->scenario == Contest::SCENARIO_OFFLINE): ?>
+        <p></p>
+        <?php if ($model->description):?>
         <div class="card">
             <div class="card-body">
-            该比赛为线下赛，如需参赛，请联系管理员。
+                <?= Yii::$app->formatter->asMarkdown($model->description) ?>
+            </div>
+        </div>
+        <?php endif; ?>
+        <p></p>
+        <?php if ($model->scenario == Contest::SCENARIO_OFFLINE): ?>
+        <div class="card">
+            <div class="card-body">
+                您尚未报名参加该比赛，请联系管理员申请参赛，或比赛结束后再来访问。
             </div>
         </div>
         <p></p>
-    <?php else: ?>
-        
+        <?php else: ?>
         <div class="card">
             <div class="card-body">
-                <h3 class="card-title">参赛协议</h3>
-                <p>一、遵守比赛规则，不与其他人分享解决方案；</p>
-                <p>二、不以任何形式破坏和攻击测评系统。</p>
+                <p>您尚未报名参加该比赛，请报名参赛或比赛结束后再来访问。</p>
+                <?= Html::a(Yii::t('app', '报名参赛'), ['/contest/register', 'id' => $model->id, 'register' => 1], ['class' => 'btn btn-success btn-block']) ?>
             </div>
         </div>
+        <?php endif; ?>
+    </div>
+    <div class="col-lg-4">
+        <div class="list-group">
+            <div class="list-group-item"><?= Yii::t('app', 'Current time') ?><span class="text-secondary float-right"
+                    id="nowdate"><?= date("Y-m-d H:i:s") ?></span></div>
+            <div class="list-group-item"><?= Yii::t('app', 'Start time') ?><span
+                    class="text-secondary float-right"><?= $model->start_time ?></span></div>
+            <div class="list-group-item"><?= Yii::t('app', 'End time') ?>
+                <?php if (strtotime($model->end_time) >= 253370736000): ?>
+                <span class="text-secondary float-right">一直开放</span>
+            </div>
+            <?php else: ?>
+            <span class="text-secondary float-right"><?= $model->end_time ?></span>
+        </div>
+        <?php endif; ?>
 
-        <p></p>
+        <div class="list-group-item"><?= Yii::t('app', 'Type') ?><span
+                class="text-secondary float-right"><?= $model->getType() ?></span></div>
+        <div class="list-group-item"><?= Yii::t('app', 'Status') ?><span
+                class="text-secondary float-right"><?= $model->getRunStatus(true) ?></span></div>
+    </div>
+</div>
 
-        <?= Html::a(Yii::t('app', 'Agree above and register'), ['/contest/register', 'id' => $model->id, 'register' => 1], ['class' => 'btn btn-block btn-outline-secondary']) ?>
-    <?php endif; ?>
 <?php endif; ?>
