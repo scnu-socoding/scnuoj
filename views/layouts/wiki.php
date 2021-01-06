ffc3d32dee08212957e0eb95e128e2e281a75a69
Bobby Rong <rjl931189261@126.com>
2021-01-06 16:33:55 +0800

    hide problemsettings related topics
    Former-commit-id: c047b9971e59346781cf2bd1e77350672ee1d055

--- views/layouts/wiki.php
+++ views/layouts/wiki.php
@@ -9,26 +9,22 @@ $this->title = Yii::t('app', 'Wiki');;
 ?>
 
 <?php $this->beginContent('@app/views/layouts/main.php'); ?>
-<div class="row">
-    <div class="col-md-4 col-lg-3">
-        <?= Nav::widget([
+
+<?= Nav::widget([
             'items' => [
-                ['label' => Yii::t('app', '判题信息'), 'url' => ['wiki/index']],
+                ['label' => Yii::t('app', '判题'), 'url' => ['wiki/index']],
                 ['label' => Yii::t('app', 'Contest'), 'url' => ['wiki/contest']],
-                ['label' => Yii::t('app', '出题要求'), 'url' => ['wiki/problem']],
-                ['label' => Yii::t('app', 'Special Judge'), 'url' => ['wiki/spj']],
-                ['label' => Yii::t('app', 'OI 模式'), 'url' => ['wiki/oi']],
+                // ['label' => Yii::t('app', '出题要求'), 'url' => ['wiki/problem']],
+                // ['label' => Yii::t('app', 'Special Judge'), 'url' => ['wiki/spj']],
+                // ['label' => Yii::t('app', 'OI 模式'), 'url' => ['wiki/oi']],
                 ['label' => Yii::t('app', 'About'), 'url' => ['wiki/about']]
             ],
-            'options' => ['class' => 'nav nav-pills flex-column']
+            'options' => ['class' => 'nav nav-pills']
         ]) ?>
-    <p></p>
-    </div>
-    <div class="col-md-8 col-lg-9">
-        <div class="wiki-contetn">
-            <?= $content ?>
-        </div>
+<p></p>
+<div class="row">
+    <div class="col">
+        <?= $content ?>
     </div>
 </div>
 <?php $this->endContent(); ?>
-
