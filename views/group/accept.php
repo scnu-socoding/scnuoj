4021381e7d8f95d5261590a3b403cf40d05309d1
Bobby Rong <rjl931189261@126.com>
2021-01-06 10:25:23 +0800

    redesign group accept page
    Former-commit-id: ecdb26d07602cb706c60a68101cb276e174560f4

--- views/group/accept.php
+++ views/group/accept.php
@@ -14,24 +14,26 @@ $this->title = Html::encode($model->name);
 // $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-<h2><?= Html::a(Html::encode($model->name), ['/group/view', 'id' => $model->id]) ?></h2>
+<h3><?= Html::encode($model->name) ?></h3>
+<p><?= Html::encode($model->description) ?></p>
 <?php if ($model->getRole() == GroupUser::ROLE_INVITING): ?>
-    <h3>邀请你加入小组：</h3>
-    <h4><?= Yii::$app->formatter->asMarkdown($model->description) ?></h4>
-    <?= Html::a('同意加入', ['/group/accept', 'id' => $model->id, 'accept' => 1], ['class' => 'btn btn-success']) ?>
-    <?= Html::a('残忍拒绝', ['/group/accept', 'id' => $model->id, 'accept' => 0], ['class' => 'btn btn-danger']) ?>
+<div class="alert alert-light"><i class="fas fa-fw fa-question-circle"></i> 是否接受邀请加入此小组？</div>
+<div class="btn-block btn-group">
+    <?= Html::a('同意', ['/group/accept', 'id' => $model->id, 'accept' => 1], ['class' => 'btn btn-success']) ?>
+    <?= Html::a('拒绝', ['/group/accept', 'id' => $model->id, 'accept' => 0], ['class' => 'btn btn-danger']) ?>
+</div>
 <?php elseif ($model->join_policy == Group::JOIN_POLICY_APPLICATION): ?>
-    <?= Html::a('申请加入', ['/group/accept', 'id' => $model->id, 'accept' => 3], ['class' => 'btn btn-success']) ?>
+<?= Html::a('申请加入', ['/group/accept', 'id' => $model->id, 'accept' => 3], ['class' => 'btn btn-success btn-block']) ?>
 <?php elseif ($model->join_policy == Group::JOIN_POLICY_FREE): ?>
-    <?= Html::a('加入小组', ['/group/accept', 'id' => $model->id, 'accept' => 2], ['class' => 'btn btn-success']) ?>
+<?= Html::a('加入小组', ['/group/accept', 'id' => $model->id, 'accept' => 2], ['class' => 'btn btn-success btn-block']) ?>
 <?php endif; ?>
 
-<hr>
-<h3>小组成员列表</h3>
+<p></p>
+
+<h5>小组成员列表</h5>
 <?= GridView::widget([
     'layout' => '{items}{pager}',
     'dataProvider' => $userDataProvider,
-    // 'tableOptions' => ['class' => 'table table-striped table-bordered'],
     'tableOptions' => ['class' => 'table'],
     'options' => ['class' => 'table-responsive'],
     'columns' => [
@@ -41,12 +43,13 @@ $this->title = Html::encode($model->name);
                 return $model->getRole(true);
             },
             'format' => 'raw',
-            'options' => ['width' => '150px']
+            'options' => ['width' => '150px'],
+            'enableSorting' => false
         ],
         [
             'attribute' => Yii::t('app', 'Nickname'),
             'value' => function ($model, $key, $index, $column) {
-                return Html::a(Html::encode($model->user->nickname), ['/user/view', 'id' => $model->user->id]);
+                return Html::a(Html::encode($model->user->nickname), ['/user/view', 'id' => $model->user->id], ['class' => 'text-dark']);
             },
             'format' => 'raw',
         ],
@@ -55,7 +58,8 @@ $this->title = Html::encode($model->name);
             'value' => function ($model, $key, $index, $column) {
                 return Yii::$app->formatter->asRelativeTime($model->created_at);
             },
-            'options' => ['width' => '150px']
+            'options' => ['width' => '150px'],
+            'enableSorting' => false
         ]
     ],
     'pager' => [
