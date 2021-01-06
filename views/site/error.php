9a5e73181ee354275aa2459709046827a4e18f95
Bobby Rong <rjl931189261@126.com>
2021-01-06 12:27:50 +0800

    redesign 404 and 403 page
    Former-commit-id: 685139ca5f71272ce806e5f437061845615b7e0b

--- views/site/error.php
+++ views/site/error.php
@@ -9,19 +9,19 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="alert alert-danger">
+    <i class="fas fa-fw fa-info-circle"></i> <?= nl2br(Html::encode($message)) ?>
+</div>
+
+<div class="card">
+    <img src="<?= Yii::getAlias('@web') . '/images/error-header.jpg' ?>" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h3 class="card-title"><?= Html::encode($this->title) ?></h3>
+        非常抱歉，页面没有如您所期望地加载，在大多数情况下可能是因为访问权限受限或者页面不存在。<br>
+        如果您认为这是站点本身的问题，欢迎您联系 <a target="_blank" href="https://github.com/scnu-socoding/scnuoj">SCNU Online Judge 开发组</a>。
+        <p></p>
+        <a class="btn-block btn btn-primary" href="<?=Yii::$app->homeUrl?>"> <i class="fas fa-fw fa-home"></i> 返回首页</a>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
