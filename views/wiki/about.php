99ba079010dedc2fd5a90b0d19c0cf81ab4d8588
Bobby Rong <rjl931189261@126.com>
2021-01-08 22:18:40 +0800

    redesign about page
    Former-commit-id: 52744f7e964ac336d76dd530c24209c086d01138

--- views/wiki/about.php
+++ views/wiki/about.php
@@ -2,44 +2,46 @@
     <img src="<?= Yii::getAlias('@web') . '/images/error-header.jpg' ?>" class="img-fluid rounded">
     <p></p>
 </div>
+
+<div class="alert alert-light">
+    <i class="fas fa-fw fa-info-circle"></i> <b>SCNUOJ</b> 可对用户在线提交的源代码进行编译和执行，并通过预先设计的测试数据检验代码的正确性。
+</div>
+<p></p>
 <div class="list-group">
 
     <div class="list-group-item">
-        <b>名称</b>
+        名称
         <span class="float-right text-secondary">
             <?=  Yii::$app->setting->get('schoolName') ?>在线评测系统
         </span>
     </div>
     <div class="list-group-item">
-        <b>SCNUOJ 版本</b>
+        SCNUOJ 版本
         <span class="float-right text-secondary">
             Version <?= date("Y.m.d", filemtime(Yii::getAlias('@app/CHANGELOG.md'))) ?>
         </span>
     </div>
+</div>
+
+<p></p>
+<div class="list-group">
     <a class="list-group-item list-group-item-action" target="_blank" href="//github.com/SCNU-SoCoding/scnuoj">
-        <b>项目网址</b>
+        项目源码
         <span class="float-right text-secondary">
             <i class="fas fa-fw fa-angle-right"></i>
         </span>
     </a>
     <a class="list-group-item list-group-item-action" target="_blank" href="//github.com/SCNU-SoCoding/scnuoj/issues">
-        <b>问题反馈</b>
+        问题反馈
         <span class="float-right text-secondary">
             <i class="fas fa-fw fa-angle-right"></i>
         </span>
     </a>
     <a class="list-group-item list-group-item-action" target="_blank"
         href="//github.com/SCNU-SoCoding/scnuoj/graphs/contributors">
-        <b>贡献者名单</b>
+        贡献者名单
         <span class="float-right text-secondary">
             <i class="fas fa-fw fa-angle-right"></i>
         </span>
     </a>
-</div>
-
-<p></p>
-<div class="list-group">
-    <div class="text-right text-secondary list-group-item">
-        SCNUOJ 的诞生离不开 JNOJ，在此特别鸣谢 JNOJ 开发组
-    </div>
 </div>
