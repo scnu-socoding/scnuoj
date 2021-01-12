b9b7779b64338f9ee6c7f60e46fb260c601416fa
Bobby Rong <rjl931189261@126.com>
2021-01-12 17:37:16 +0800

    redesign group item page
    Former-commit-id: 04b395fb06ee47bdbebb1916bf7dd042ce53fc0c

--- views/group/_group_item.php
+++ views/group/_group_item.php
@@ -2,8 +2,9 @@
 
 use yii\helpers\Html;
 ?>
-<div class="card">
-    <div class="card-body" style="padding: 1rem;">
+<?php if ($model['id'] != 1): ?>
+<div class="card" style="border-color:#4582ec;">
+    <div class="card-body">
         <h5 style="margin-bottom: 0.25rem;">
             <?= Html::a(Html::encode($model['name']), ['/group/view', 'id' => $model['id']]) ?>
         </h5>
@@ -14,3 +15,17 @@ use yii\helpers\Html;
         <?php endif; ?>
     </div>
 </div>
+<?php else:?>
+    <div class="card" style="border-color:goldenrod;">
+    <div class="card-body">
+        <h5 style="margin-bottom: 0.25rem;">
+            <?= Html::a(Html::encode($model['name']), ['/group/view', 'id' => $model['id']], ['style' => 'color:goldenrod;']) ?>
+        </h5>
+        <?php if ($model['description']) : ?>
+            <small>
+                <?= Html::encode($model['description']) ?>
+            </small>
+        <?php endif; ?>
+    </div>
+</div>
+<?php endif;?>
