83d878910bdee85d3078f27654cdcc80f0998f2a
Bobby Rong <rjl931189261@126.com>
2021-01-12 17:52:57 +0800

    initial contest search frame
    Former-commit-id: 693608732f1d68e5c212776691a921bb9b6c185f

--- views/contest/_search.php
+++ views/contest/_search.php
@@ -8,56 +8,29 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="contest-search">
+<?php $form = ActiveForm::begin([
+    'action' => ['index'],
+    'method' => 'get',
+    'options' => [
+        'class' => ''
+    ],
+]); ?>
+<div class="row">
 
-    <?php $form = ActiveForm::begin([
-        'action' => ['index'],
-        'method' => 'get',
-    ]); ?>
 
-    <?= $form->field($model, 'cid') ?>
+    <div class="col-lg-8" style="margin-bottom: 1rem; padding-right:5px; padding-left:5px;">
+        <?= $form->field($model, 'title', [
+            'template' => "<div class=\"input-group\">{input}</div>",
+            'options' => ['class' => ''],
+        ])->textInput(['maxlength' => 128, 'autocomplete' => 'off', 'placeholder' => Yii::t('app', 'Title')])->label(false) ?>
 
-    <?= $form->field($model, 'title') ?>
-
-    <?= $form->field($model, 'description') ?>
-
-    <?= $form->field($model, 'start_time') ?>
-
-    <?php // echo $form->field($model, 'end_time') ?>
-
-    <?php // echo $form->field($model, 'lock_board_time') ?>
-
-    <?php // echo $form->field($model, 'hide_others') ?>
-
-    <?php // echo $form->field($model, 'board_make') ?>
-
-    <?php // echo $form->field($model, 'isvirtual') ?>
-
-    <?php // echo $form->field($model, 'owner') ?>
-
-    <?php // echo $form->field($model, 'report') ?>
-
-    <?php // echo $form->field($model, 'mboard_make') ?>
-
-    <?php // echo $form->field($model, 'allp') ?>
-
-    <?php // echo $form->field($model, 'type') ?>
-
-    <?php // echo $form->field($model, 'has_cha') ?>
-
-    <?php // echo $form->field($model, 'challenge_end_time') ?>
-
-    <?php // echo $form->field($model, 'challenge_start_time') ?>
-
-    <?php // echo $form->field($model, 'password') ?>
-
-    <?php // echo $form->field($model, 'owner_viewable') ?>
-
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
-
+    <div class="col-lg-4" style="margin-bottom: 1rem; padding-right:5px; padding-left:5px;">
+        <div class="btn-group btn-block">
+            <?= Html::submitButton('<i class="fas fa-fw fa-search"></i> ' . Yii::t('app', 'Search'), ['class' => 'btn btn-info']) ?>
+            <?= Html::resetButton('<i class="fas fa-fw fa-history"></i> ' . Yii::t('app', 'Reset'), ['class' => 'btn btn-primary']) ?>
+        </div>
+    </div>
 </div>
+<?php ActiveForm::end(); ?>
