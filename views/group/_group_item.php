04cbbecacacac27c9cb78ea705ee2322a1522b6c
Bobby Rong <rjl931189261@126.com>
2021-01-12 16:38:03 +0800

    redesign group item layout
    Former-commit-id: 941aa854c4af3d7f525b5dcffda89d1174ba0174

--- views/group/_group_item.php
+++ views/group/_group_item.php
@@ -1,14 +1,16 @@
 <?php
+
 use yii\helpers\Html;
 ?>
 <div class="card">
-    <div class="card-header">
-        <?= Html::a(Html::encode($model['name']), ['/group/view', 'id' => $model['id']]) ?>
-    </div>
-    <div class="card-body">
-        <p class="card-text">
-            <?= Html::encode($model['description']) ?>
-        </p>
+    <div class="card-body" style="padding: 1rem;">
+        <h5 style="margin-bottom: 0.25rem;">
+            <?= Html::a(Html::encode($model['name']), ['/group/view', 'id' => $model['id']]) ?>
+        </h5>
+        <?php if ($model['description']) : ?>
+            <small>
+                <?= Html::encode($model['description']) ?>
+            </small>
+        <?php endif; ?>
     </div>
 </div>
-
