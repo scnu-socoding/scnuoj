2dbb5d5f77569d9ba2ea59585fa2d4ef591def1c
Bobby Rong <rjl931189261@126.com>
2021-01-15 18:17:07 +0800

    fix missing icon in test_data page
    Former-commit-id: 6223bd586f0974fde2a7cedeccc8aacca91f23d1

--- modules/admin/views/problem/test_data.php
+++ modules/admin/views/problem/test_data.php
@@ -31,93 +31,89 @@ $this->params['model'] = $model;
 
     <div class="row table-responsive">
         <div class="col-md-12">
-            <?php if (extension_loaded('zip')): ?>
+            <?php if (extension_loaded('zip')) : ?>
                 <p>
                     <?= Html::a('下载全部数据', ['download-data', 'id' => $model->id], ['class' => 'btn btn-success']); ?>
                 </p>
-            <?php else: ?>
+            <?php else : ?>
                 <p>
                     服务器未启用 php-zip 扩展，如需下载测试数据，请安装 php-zip　扩展。
                 </p>
             <?php endif; ?>
         </div>
-        <div class="col-md-6">
-            <table class="table table-bordered table-rank">
-                <caption>
-                    标准输入文件
-                    <a href="<?= Url::toRoute(['/admin/problem/deletefile', 'id' => $model->id,'name' => 'in']) ?>" onclick="return confirm('确定删除全部输入文件？');">
-                        删除全部输入文件
-                    </a>
-                </caption>
-                <tr>
-                    <th>文件名</th>
-                    <th>大小(bytes)</th>
-                    <th>修改时间</th>
-                    <th>操作</th>
-                </tr>
-                <?php foreach ($files as $file): ?>
-                    <?php
-                    if (!strpos($file['name'], '.in'))
-                        continue;
-                    ?>
+        <div class="row">
+            <div class="col-md-6">
+                <table class="table table-bordered table-rank">
+                    <caption>
+                        标准输入文件
+                        <a href="<?= Url::toRoute(['/admin/problem/deletefile', 'id' => $model->id, 'name' => 'in']) ?>" onclick="return confirm('确定删除全部输入文件？');">
+                            删除全部输入文件
+                        </a>
+                    </caption>
                     <tr>
-                        <th><?= $file['name'] ?></th>
-                        <th><?= $file['size'] ?></th>
-                        <th><?= date('Y-m-d H:i', $file['time']) ?></th>
-                        <th>
-                            <a href="<?= Url::toRoute(['/admin/problem/viewfile', 'id' => $model->id,'name' => $file['name']]) ?>"
-                               target="_blank"
-                               title="<?= Yii::t('app', 'View') ?>">
-                                <span class="glyphicon glyphicon-eye-open"></span>
-                            </a>
-                            &nbsp;
-                            <a href="<?= Url::toRoute(['/admin/problem/deletefile', 'id' => $model->id,'name' => $file['name']]) ?>"
-                               title="<?= Yii::t('app', 'Delete') ?>">
-                                <span class="glyphicon glyphicon-remove"></span>
-                            </a>
-                        </th>
+                        <th>文件名</th>
+                        <th>大小(bytes)</th>
+                        <th>修改时间</th>
+                        <th>操作</th>
                     </tr>
-                <?php endforeach; ?>
-            </table>
-        </div>
-        <div class="col-md-6">
-            <table class="table table-bordered table-rank">
-                <caption>
-                    标准输出文件
-                    <a href="<?= Url::toRoute(['/admin/problem/deletefile', 'id' => $model->id, 'name' => 'out']) ?>" onclick="return confirm('确定删除全部输出文件？');">
-                        删除全部输出文件
-                    </a>
-                </caption>
-                <tr>
-                    <th>文件名</th>
-                    <th>大小(bytes)</th>
-                    <th>修改时间</th>
-                    <th>操作</th>
-                </tr>
-                <?php foreach ($files as $file): ?>
-                    <?php
-                    if (!strpos($file['name'], '.out') && !strpos($file['name'], '.ans'))
-                        continue;
-                    ?>
+                    <?php foreach ($files as $file) : ?>
+                        <?php
+                        if (!strpos($file['name'], '.in'))
+                            continue;
+                        ?>
+                        <tr>
+                            <th><?= $file['name'] ?></th>
+                            <th><?= $file['size'] ?></th>
+                            <th><?= date('Y-m-d H:i', $file['time']) ?></th>
+                            <th>
+                                <a href="<?= Url::toRoute(['/admin/problem/viewfile', 'id' => $model->id, 'name' => $file['name']]) ?>" target="_blank" title="<?= Yii::t('app', 'View') ?>">
+                                    <span class="fas fa-sm fa-trash"></span>
+                                </a>
+                                &nbsp;
+                                <a href="<?= Url::toRoute(['/admin/problem/deletefile', 'id' => $model->id, 'name' => $file['name']]) ?>" title="<?= Yii::t('app', 'Delete') ?>">
+                                    <span class="fas fa-sm fa-eye"></span>
+                                </a>
+                            </th>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+            <div class="col-md-6">
+                <table class="table table-bordered table-rank">
+                    <caption>
+                        标准输出文件
+                        <a href="<?= Url::toRoute(['/admin/problem/deletefile', 'id' => $model->id, 'name' => 'out']) ?>" onclick="return confirm('确定删除全部输出文件？');">
+                            删除全部输出文件
+                        </a>
+                    </caption>
                     <tr>
-                        <th><?= $file['name'] ?></th>
-                        <th><?= $file['size'] ?></th>
-                        <th><?= date('Y-m-d H:i', $file['time']) ?></th>
-                        <th>
-                            <a href="<?= Url::toRoute(['/admin/problem/viewfile', 'id' => $model->id,'name' => $file['name']]) ?>"
-                               target="_blank"
-                               title="<?= Yii::t('app', 'View') ?>">
-                                <span class="glyphicon glyphicon-eye-open"></span>
-                            </a>
-                            &nbsp;
-                            <a href="<?= Url::toRoute(['/admin/problem/deletefile', 'id' => $model->id,'name' => $file['name']]) ?>"
-                               title="<?= Yii::t('app', 'Delete') ?>">
-                                <span class="glyphicon glyphicon-remove"></span>
-                            </a>
-                        </th>
+                        <th>文件名</th>
+                        <th>大小(bytes)</th>
+                        <th>修改时间</th>
+                        <th>操作</th>
                     </tr>
-                <?php endforeach; ?>
-            </table>
+                    <?php foreach ($files as $file) : ?>
+                        <?php
+                        if (!strpos($file['name'], '.out') && !strpos($file['name'], '.ans'))
+                            continue;
+                        ?>
+                        <tr>
+                            <th><?= $file['name'] ?></th>
+                            <th><?= $file['size'] ?></th>
+                            <th><?= date('Y-m-d H:i', $file['time']) ?></th>
+                            <th>
+                                <a href="<?= Url::toRoute(['/admin/problem/viewfile', 'id' => $model->id, 'name' => $file['name']]) ?>" target="_blank" title="<?= Yii::t('app', 'View') ?>">
+                                    <span class="fas fa-sm fa-eye"></span>
+                                </a>
+                                &nbsp;
+                                <a href="<?= Url::toRoute(['/admin/problem/deletefile', 'id' => $model->id, 'name' => $file['name']]) ?>" title="<?= Yii::t('app', 'Delete') ?>">
+                                    <span class="fas fa-sm fa-trash"></span>
+                                </a>
+                            </th>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
         </div>
     </div>
 </div>
