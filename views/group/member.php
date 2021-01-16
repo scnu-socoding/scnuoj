6f46617083bf6c0174e76a999c3ec71ac21bb617
Bobby Rong <rjl931189261@126.com>
2021-01-16 11:59:03 +0800

    tiny change

--- views/group/member.php
+++ views/group/member.php
@@ -28,7 +28,7 @@ $scoreboardFrozenTime = Yii::$app->setting->get('scoreboardFrozenTime') / 3600;
 <h3><?= Html::encode($this->title) ?></h3>
 
 <?php
-    $menuItems = [
+$menuItems = [
     [
         'label' => Yii::t('app', 'Information'),
         'url' => ['group/view', 'id' => $model->id],
@@ -41,15 +41,15 @@ $scoreboardFrozenTime = Yii::$app->setting->get('scoreboardFrozenTime') / 3600;
         'label' => Yii::t('app', 'Member'),
         'url' => ['group/member', 'id' => $model->id],
     ]
-    ];
-    echo Nav::widget([
-        'items' => $menuItems,
-        'options' => ['class' => 'nav nav-pills hidden-print'],
-        'encodeLabels' => false
-    ]) ?>
+];
+echo Nav::widget([
+    'items' => $menuItems,
+    'options' => ['class' => 'nav nav-pills hidden-print'],
+    'encodeLabels' => false
+]) ?>
 <p></p>
 
-<?php if ($model->hasPermission()): ?>
+<?php if ($model->hasPermission()) : ?>
     <?php Modal::begin([
         'title' => Yii::t('app', 'Invite Member'),
         'toggleButton' => [
@@ -57,15 +57,15 @@ $scoreboardFrozenTime = Yii::$app->setting->get('scoreboardFrozenTime') / 3600;
             'tag' => 'a',
             'class' => 'btn btn-outline-danger btn-block'
         ]
-            ]); ?>
+    ]); ?>
     <?php $form = ActiveForm::begin(); ?>
     <p class="hint-block">1. 一个用户占据一行，每行格式为<code>username</code>。</p>
     <p class="hint-block">2. 必须是已经注册过的用户。</p>
     <?= $form->field($newGroupUser, 'username')->textarea(['rows' => 10]) ?>
-    <?php if (Yii::$app->setting->get('isGroupJoin')): ?>
-    <?= $form->field($newGroupUser, 'role')->radioList(['2'=>'邀请中','4'=>'普通成员'],['value'=>[4]]) ?>
-    <?php else: ?>
-    <?= $form->field($newGroupUser, 'role')->radioList(['2'=>'邀请中'],['value'=>[2]]) ?>
+    <?php if (Yii::$app->setting->get('isGroupJoin')) : ?>
+        <?= $form->field($newGroupUser, 'role')->radioList(['2' => '邀请中', '4' => '普通成员'], ['value' => [4]]) ?>
+    <?php else : ?>
+        <?= $form->field($newGroupUser, 'role')->radioList(['2' => '邀请中'], ['value' => [2]]) ?>
     <?php endif; ?>
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']) ?>
@@ -81,62 +81,62 @@ $scoreboardFrozenTime = Yii::$app->setting->get('scoreboardFrozenTime') / 3600;
         'layout' => '{items}{pager}',
         'dataProvider' => $userDataProvider,
         // 'tableOptions' => ['class' => 'table table-striped table-bordered'],
-         'tableOptions' => ['class' => 'table'],
+        'tableOptions' => ['class' => 'table'],
         'options' => ['class' => 'table-responsive'],
         'columns' => [
             [
-        'attribute' => 'role',
-        'value' => function ($model, $key, $index, $column) {
-            return $model->getRole(true);
-        },
-        'format' => 'raw',
-        'options' => ['style' => 'min-width:100px'],
-        'enableSorting' => false
+                'attribute' => 'role',
+                'value' => function ($model, $key, $index, $column) {
+                    return $model->getRole(true);
+                },
+                'format' => 'raw',
+                'options' => ['style' => 'width:100px;min-width:100px'],
+                'enableSorting' => false
             ],
             [
-        'attribute' => Yii::t('app', 'Nickname'),
-        'value' => function ($model, $key, $index, $column) {
-            return Html::a(Html::encode($model->user->nickname), ['/user/view', 'id' => $model->user->id], ['class' => 'text-dark']);
-        },
-        'format' => 'raw',
-        'options' => ['style' => 'min-width:200px'],
-        'enableSorting' => false
+                'attribute' => Yii::t('app', 'Nickname'),
+                'value' => function ($model, $key, $index, $column) {
+                    return Html::a(Html::encode($model->user->nickname), ['/user/view', 'id' => $model->user->id], ['class' => 'text-dark']);
+                },
+                'format' => 'raw',
+                'options' => ['style' => 'min-width:200px'],
+                'enableSorting' => false
             ],
             [
-        'attribute' => 'created_at',
-        'value' => function ($model, $key, $index, $column) {
-            return Yii::$app->formatter->asRelativeTime($model->created_at);
-        },
-        'options' => ['style' => 'min-width:100px'],
-        'enableSorting' => false
+                'attribute' => 'created_at',
+                'value' => function ($model, $key, $index, $column) {
+                    return Yii::$app->formatter->asRelativeTime($model->created_at);
+                },
+                'options' => ['style' => 'min-width:100px;width:100px'],
+                'enableSorting' => false
             ],
             [
-        'class' => 'yii\grid\ActionColumn',
-        'template' => '{user-update} {user-delete}',
-        'buttons' => [
-            'user-update' => function ($url, $model, $key) {
-                $options = [
-            'title' => Yii::t('yii', 'Update'),
-            'aria-label' => Yii::t('yii', 'Update'),
-            'onclick' => 'return false',
-            'data-click' => "user-manager",
-            'class' => 'text-dark'
-                ];
-                return Html::a('<i class="fas fa-sm fa-pen"></i>', $url, $options);
-            },
-            'user-delete' => function ($url, $model, $key) {
-                $options = [
-            'title' => Yii::t('yii', 'Delete'),
-            'aria-label' => Yii::t('yii', 'Delete'),
-            'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-            'data-method' => 'post',
-            'class' => 'text-dark'
-                ];
-                return Html::a('<span class="fas fa-sm fa-trash"></span>', $url, $options);
-            }
-        ],
-        'visible' => $model->hasPermission(),
-        'options' => ['width' => '90px']
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{user-update} {user-delete}',
+                'buttons' => [
+                    'user-update' => function ($url, $model, $key) {
+                        $options = [
+                            'title' => Yii::t('yii', 'Update'),
+                            'aria-label' => Yii::t('yii', 'Update'),
+                            'onclick' => 'return false',
+                            'data-click' => "user-manager",
+                            'class' => 'text-dark'
+                        ];
+                        return Html::a('<i class="fas fa-sm fa-pen"></i>', $url, $options);
+                    },
+                    'user-delete' => function ($url, $model, $key) {
+                        $options = [
+                            'title' => Yii::t('yii', 'Delete'),
+                            'aria-label' => Yii::t('yii', 'Delete'),
+                            'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                            'data-method' => 'post',
+                            'class' => 'text-dark'
+                        ];
+                        return Html::a('<span class="fas fa-sm fa-trash"></span>', $url, $options);
+                    }
+                ],
+                'visible' => $model->hasPermission(),
+                'options' => ['style' => 'min-width:60px;width:60px'],
             ]
         ],
         'pager' => [
