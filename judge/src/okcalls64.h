0806060861fc3e4e80aee679fc9ef2e2fb77443d
Bobby Rong <rjl931189261@126.com>
2021-01-17 11:32:33 +0800

    allow SYS_rt_sigaction in okcalls64

--- judge/src/okcalls64.h
+++ judge/src/okcalls64.h
@@ -18,7 +18,7 @@
  */
 //c & c++
 int LANG_CV[CALL_ARRAY_SIZE] = {
-	SYS_dup3, SYS_exit,
+	SYS_dup3, SYS_exit, SYS_rt_sigaction,
 	SYS_read, SYS_write, SYS_mprotect, SYS_munmap, SYS_brk, SYS_arch_prctl, SYS_pread64, SYS_open, SYS_writev,
 	SYS_time, SYS_futex, SYS_set_thread_area, SYS_access, SYS_clock_gettime, SYS_exit_group, SYS_mq_open,
 	SYS_ioprio_get, SYS_unshare, SYS_set_robust_list, SYS_splice, SYS_close, SYS_stat, SYS_fstat, SYS_execve,
