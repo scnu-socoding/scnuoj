2e14740ea0139951391af66027bde4f8bf646366
Bobby Rong <rjl931189261@126.com>
2021-01-17 11:50:58 +0800

    update okcalls

--- judge/src/okcalls64.h
+++ judge/src/okcalls64.h
@@ -18,10 +18,10 @@
  */
 //c & c++
 int LANG_CV[CALL_ARRAY_SIZE] = {
-	SYS_dup3, SYS_exit, SYS_rt_sigaction,
-	SYS_read, SYS_write, SYS_mprotect, SYS_munmap, SYS_brk, SYS_arch_prctl, SYS_pread64, SYS_open, SYS_writev,
+	SYS_read, SYS_write, SYS_mprotect, SYS_arch_prctl, SYS_munmap, SYS_brk, SYS_pread64, SYS_open,
+	SYS_dup3, SYS_exit, SYS_rt_sigaction, SYS_execve, SYS_writev,
 	SYS_time, SYS_futex, SYS_set_thread_area, SYS_access, SYS_clock_gettime, SYS_exit_group, SYS_mq_open,
-	SYS_ioprio_get, SYS_unshare, SYS_set_robust_list, SYS_splice, SYS_close, SYS_stat, SYS_fstat, SYS_execve,
+	SYS_ioprio_get, SYS_unshare, SYS_set_robust_list, SYS_splice, SYS_close, SYS_stat, SYS_fstat, 
 	SYS_uname, SYS_lseek, SYS_readlink, SYS_mmap, SYS_sysinfo, 0x1FF, 0};
 //java
 int LANG_JV[CALL_ARRAY_SIZE] = {
