0a73c04a2cb6d222ea033254f9ba5a77829639fb
Bobby Rong <rjl931189261@126.com>
2021-01-25 14:15:37 +0800

    use html::encode() in print view

--- views/print/view.php
+++ views/print/view.php
@@ -38,13 +38,13 @@ $this->title = '查看打印详情';
                     <?= $model->id ?>
                 </td>
                 <td>
-                    <?= $model->contest->title ?>
+                <?= Html::a(Html::encode($model->contest->title), ['/contest/view', 'id' => $model->contest_id], ['class' => 'text-dark']); ?>
                 </td>
                 <td>
-                    <?= $model->user->nickname ?>
+                    <?= Html::a(Html::encode($model->user->nickname), ['/user/view', 'id' => $model->user->id], ['class' => 'text-dark']); ?>
                 </td>
                 <td>
-                    <?= $model->user->username ?>
+                <?= Html::a(Html::encode($model->user->username), ['/user/view', 'id' => $model->user->id], ['class' => 'text-dark']); ?>
                 </td>
                 <td>
                     <?= Yii::$app->formatter->asRelativeTime($model->created_at) ?>
