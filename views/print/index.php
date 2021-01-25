75feb8366e85f83a31667269a2ac102faaf74f4f
Bobby Rong <rjl931189261@126.com>
2021-01-25 14:35:33 +0800

    update print views for admins

--- views/print/index.php
+++ views/print/index.php
@@ -16,7 +16,7 @@ $this->title = 'Print Sources';
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('打印代码', ['create', 'id' => $contest->id], ['class' => 'btn btn-success']) ?> 如需打印代码以供队友查看，请点击此按钮提交，工作人员打印好后会送至队伍前。
+        <?= Html::a('新建', ['create', 'id' => $contest->id], ['class' => 'btn btn-success']) ?> 在此处管理比赛的待打印代码，请注意查看某份代码后将自动标记代码为已处理。
     </p>
 
     <?= GridView::widget([
@@ -52,7 +52,37 @@ $this->title = 'Print Sources';
             ],
             'created_at:datetime',
             [
-                'class' => 'yii\grid\ActionColumn'
+                'class' => 'yii\grid\ActionColumn',
+                'controller' => 'print',
+                'template' => '{view} {update} {delete}',
+                'buttons' => [
+                    'view' => function ($url, $model, $key) {
+                        $options = [
+                            'title' => Yii::t('yii', 'View'),
+                            'aria-label' => Yii::t('yii', 'View'),
+                            'class' => 'text-dark'
+                        ];
+                        return Html::a('<i class="fas fa-sm fa-eye"></i>', $url, $options);
+                    },
+                    'update' => function ($url, $model, $key) {
+                        $options = [
+                            'title' => Yii::t('yii', 'Update'),
+                            'aria-label' => Yii::t('yii', 'Update'),
+                            'class' => 'text-dark'
+                        ];
+                        return Html::a('<i class="fas fa-sm fa-pen"></i>', $url, $options);
+                    },
+                    'delete' => function ($url, $model, $key) {
+                        $options = [
+                            'title' => Yii::t('yii', 'Delete'),
+                            'aria-label' => Yii::t('yii', 'Delete'),
+                            'data-confirm' => Yii::t('yii', '【注意】你正在删除选手提交打印的代码，查看过的代码会自动更新状态，因此【非必须情况，请不要删除】。删除前请确认你已经打印了此份代码。'),
+                            'data-method' => 'post',
+                            'class' => 'text-dark'
+                        ];
+                        return Html::a('<span class="fas fa-sm fa-trash"></span>', $url, $options);
+                    }
+                ]
             ],
         ],
     ]); ?>
