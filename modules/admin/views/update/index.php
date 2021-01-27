d29c0685e078e38fd88c079e6f02e21632b1ea80
Bobby Rong <rjl931189261@126.com>
2021-01-27 23:01:59 +0800

    redesign oj update page

--- modules/admin/views/update/index.php
+++ modules/admin/views/update/index.php
@@ -1,26 +1,22 @@
 <?php
 
-
 /* @var $this yii\web\View */
 /* @var $settings array */
 /* @var $form yii\widgets\ActiveForm */
 $this->title = Yii::t('app', 'Update');
 ?>
-<h1>当前版本：<?= date("Y.m.d", filemtime(Yii::getAlias('@app/CHANGELOG.md'))) ?></h1>
-<p>更新方法：暂无</p>
-<p>
-    如果你在使用过程中发现 Bug，或者希望增加一些额外的功能，欢迎使用
-    <a href="https://github.com/SCNU-SoCoding/scnuoj/issues" target="_blank">GitHub Issues</a> 来报告．
-</p>
-<p>项目主页：<a href="https://github.com/SCNU-SoCoding/scnuoj" target="_blank">SCNUOJ GitHub Project</a></p>
-<hr>
-<p>
-    提示：当前页面打开可能会有些慢，是因为为了保证可以及时看到开发的变化，
-    以下内容是读取自
-    <a href="https://git.lug.ustc.edu.cn/bobby285271/scnuoj/-/raw/master/CHANGELOG.md" target="_blank">https://git.lug.ustc.edu.cn/bobby285271/scnuoj/-/raw/master/CHANGELOG.md</a>
-    这个链接下的文件，在读取过程中访问慢导致的。
-</p>
-<hr>
-<div>
-    <?= Yii::$app->formatter->asMarkdown($changelog,true) ?>
+<p class="lead">感谢使用 SCNUOJ <?= date("Y.m.d", filemtime(Yii::getAlias('@app/CHANGELOG.md'))) ?>。</p>
+
+<a class="btn btn-outline-primary btn-block" href="https://github.com/SCNU-SoCoding/scnuoj/blob/master/docs/upgrade.md">了解如何更新</a>
+
+<p></p>
+
+<p class="lead">更新日志</p>
+
+<div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 更新日志从 <a href="https://git.lug.ustc.edu.cn/bobby285271/scnuoj/-/raw/master/CHANGELOG.md">git.lug.ustc.edu.cn</a> 抓取，感谢 USTCLUG 的代码仓库托管服务。</div>
+
+<div class="card">
+    <div class="card-body">
+        <?= Yii::$app->formatter->asMarkdown($changelog, true) ?>
+    </div>
 </div>
