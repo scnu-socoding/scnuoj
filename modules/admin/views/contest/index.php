5f9ac83ef3fcc73965637dfa54e666c265bb4e8b
Bobby Rong <rjl931189261@126.com>
2021-01-27 13:39:56 +0800

    redesign contest index for jury interface

--- modules/admin/views/contest/index.php
+++ modules/admin/views/contest/index.php
@@ -7,33 +7,45 @@ use yii\grid\GridView;
 
 $this->title = Yii::t('app', 'Contests');
 ?>
-<div class="contest-index">
+<div>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <p class="lead">创建和管理公共比赛与题目集。</p>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Contest'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+
+    <?= Html::a(Yii::t('app', 'Create Contest'), ['create'], ['class' => 'btn btn-outline-primary btn-block']) ?>
+    <p></p>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
+        'tableOptions' => ['class' => 'table table-bordered'],
+        'options' => ['class' => 'table-responsive'],
+        'layout' => '{items}{pager}',
         'columns' => [
             [
                 'attribute' => 'id',
+                'label' => '#',
                 'value' => function ($model, $key, $index, $column) {
                     return Html::a($model->id, ['contest/view', 'id' => $key]);
                 },
-                'format' => 'raw'
+                'format' => 'raw',
+                'enableSorting' => false,
             ],
             [
                 'attribute' => 'title',
                 'value' => function ($model, $key, $index, $column) {
                     return Html::a(Html::encode($model->title), ['contest/view', 'id' => $key]);
                 },
-                'format' => 'raw'
+                'format' => 'raw',
+                'enableSorting' => false,
             ],
-            'start_time',
-            'end_time',
+            [
+                'attribute' =>'start_time',
+                'enableSorting' => false,
+            ],
+            [
+                'attribute' => 'end_time',
+                'enableSorting' => false,
+            ],
             [
                 'attribute' => 'status',
                 'value' => function ($model, $key, $index, $column) {
@@ -46,48 +58,40 @@ $this->title = Yii::t('app', 'Contests');
                     }
                 },
                 'format' => 'raw',
+                'enableSorting' => false,
             ],
             [
-                'attribute' => 'scenario',
-                'value' => function ($model, $key, $index, $column) {
-                    if ($model->scenario == $model::SCENARIO_ONLINE) {
-                        return Yii::t('app', 'Online');
-                    } else {
-                        return Yii::t('app', 'Offline');
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{view} {update} {delete}',
+                'buttons' => [
+                    'view' => function ($url, $model, $key) {
+                        $options = [
+                            'title' => Yii::t('yii', 'View'),
+                            'aria-label' => Yii::t('yii', 'View'),
+                            'class' => 'text-dark'
+                        ];
+                        return Html::a('<i class="fas fa-sm fa-eye"></i>', $url, $options);
+                    },
+                    'update' => function ($url, $model, $key) {
+                        $options = [
+                            'title' => Yii::t('yii', 'Update'),
+                            'aria-label' => Yii::t('yii', 'Update'),
+                            'class' => 'text-dark'
+                        ];
+                        return Html::a('<i class="fas fa-sm fa-pen"></i>', $url, $options);
+                    },
+                    'delete' => function ($url, $model, $key) {
+                        $options = [
+                            'title' => Yii::t('yii', 'Delete'),
+                            'aria-label' => Yii::t('yii', 'Delete'),
+                            'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                            'data-method' => 'post',
+                            'class' => 'text-dark'
+                        ];
+                        return Html::a('<span class="fas fa-sm fa-trash"></span>', $url, $options);
                     }
-                },
-                'format' => 'raw',
-            ],
-            ['class' => 'yii\grid\ActionColumn',
-            'template' => '{view} {update} {delete}',
-            'buttons' => [
-                'view' => function ($url, $model, $key) {
-                    $options = [
-                        'title' => Yii::t('yii', 'View'),
-                        'aria-label' => Yii::t('yii', 'View'),
-                        'class' => 'text-dark'
-                    ];
-                    return Html::a('<i class="fas fa-sm fa-eye"></i>', $url, $options);
-                },
-                'update' => function ($url, $model, $key) {
-                    $options = [
-                        'title' => Yii::t('yii', 'Update'),
-                        'aria-label' => Yii::t('yii', 'Update'),
-                        'class' => 'text-dark'
-                    ];
-                    return Html::a('<i class="fas fa-sm fa-pen"></i>', $url, $options);
-                },
-                'delete' => function ($url, $model, $key) {
-                    $options = [
-                        'title' => Yii::t('yii', 'Delete'),
-                        'aria-label' => Yii::t('yii', 'Delete'),
-                        'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                        'data-method' => 'post',
-                        'class' => 'text-dark'
-                    ];
-                    return Html::a('<span class="fas fa-sm fa-trash"></span>', $url, $options);
-                }
-            ]]
+                ]
+            ]
         ],
     ]); ?>
 </div>
