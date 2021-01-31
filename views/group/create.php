85814aa9584208c197fbf029dc17e0959f52ddf5
Bobby Rong <rjl931189261@126.com>
2021-01-31 11:18:41 +0800

    throw 403 when no access to group create page

--- views/group/create.php
+++ views/group/create.php
@@ -14,32 +14,11 @@ $this->title = Yii::t('app', 'Create Group');
 // $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Groups'), 'url' => ['index']];
 // $this->params['breadcrumbs'][] = $this->title;
 
-$DefGp = false;
-if(Yii::$app->user->isGuest || Yii::$app->setting->get('isDefGroup') == 0)
-{
-    $DefGp = false; 
-}
-elseif ((Yii::$app->setting->get('isDefGroup')==2) && (Yii::$app->user->identity->role === User::ROLE_ADMIN) ) {
-    $DefGp = true; 
-}
-elseif(Yii::$app->setting->get('isDefGroup')==3 && (Yii::$app->user->identity->role === User::ROLE_ADMIN || Yii::$app->user->identity->role === User::ROLE_VIP)){
-    $DefGp = true; 
-}
-else{
-    $DefGp = false;  
-}
-
-
-
 ?>
 <div class="group-create">
-    <?php if ($DefGp): ?>    
-    <h1><?= Html::encode($this->title) ?></h1>
+    <p class="lead">创建一个新的小组。</p>
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
-    <?php else: ?>
-       <h3> 没有创建小组的权限！</h3>
-    <?php endif; ?>   
 </div>
