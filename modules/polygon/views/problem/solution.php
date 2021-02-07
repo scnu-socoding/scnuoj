aa4f5296407990689be7a286e73aca02db1083ea
Bobby Rong <rjl931189261@126.com>
2021-02-07 19:35:25 +0800

    sync layout for polygon pages

--- modules/polygon/views/problem/solution.php
+++ modules/polygon/views/problem/solution.php
@@ -14,16 +14,22 @@ $this->params['model'] = $model;
 
 $model->setSamples();
 ?>
-<p>
-    请在此页面提供一个“标程”（即解答该问题的正确代码程序）。它将被用来生成测试数据的标准输出。
-</p>
+
+<div class="alert alert-light">
+    <i class="fas fa-fw fa-info-circle"></i> 请在此页面提供一个标程，即解答该问题的正确代码程序。标程将被用来生成测试数据的标准输出。
+</div>
+
 <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'solution_lang')->dropDownList(Solution::getLanguageList()) ?>
+<?= $form->field($model, 'solution_lang', [
+        'template' => "{input}",
+    ])->dropDownList(Solution::getLanguageList(), ['class' => 'form-control custom-select selectpicker']) ?>
 
-    <?= $form->field($model, 'solution_source')->widget('app\widgets\codemirror\CodeMirror'); ?>
+<?= $form->field($model, 'solution_source', [
+        'template' => "{input}",
+    ])->widget('app\widgets\codemirror\CodeMirror'); ?>
 
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-primary']) ?>
-    </div>
+<div class="form-group">
+    <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success btn-block']) ?>
+</div>
 <?php ActiveForm::end(); ?>
