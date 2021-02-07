2988048f1b5f58246130e4934046b7aa820df1c1
Bobby Rong <rjl931189261@126.com>
2021-02-07 21:16:02 +0800

    redesign subtask page

--- modules/polygon/views/problem/subtask.php
+++ modules/polygon/views/problem/subtask.php
@@ -12,29 +12,19 @@ $this->params['model'] = $model;
 
 $model->setSamples();
 ?>
-<p>
-    如果题目需要配置子任务的，可以在下面填写子任务的配置。参考：<?= Html::a('子任务配置要求', ['/wiki/oi']) ?>
-</p>
+<div class="alert alert-light">
+    <i class="fas fa-fw fa-info-circle"></i> 如果题目需要配置子任务的，可以在下面填写子任务的配置，配置仅在主题库生效。
+</div>
 
-<?php if (Yii::$app->setting->get('oiMode')): ?>
-    <hr>
+<?= Html::beginForm() ?>
 
-    <p>
-        注：当前尚未开发验证配置文件正确性的功能，在 polygon 的验题中也尚未支持子任务运行，如需测试子任务的运行情况，需要同步到后台题库中测试。
-    </p>
+<div class="form-group">
+    <?= Html::label(Yii::t('app', 'Subtask'), 'subtaskContent', ['class' => 'sr-only']) ?>
 
-    <?= Html::beginForm() ?>
+    <?= \app\widgets\codemirror\CodeMirror::widget(['name' => 'subtaskContent', 'value' => $subtaskContent]);  ?>
+</div>
 
-    <div class="form-group">
-        <?= Html::label(Yii::t('app', 'Subtask'), 'subtaskContent', ['class' => 'sr-only']) ?>
-
-        <?= \app\widgets\codemirror\CodeMirror::widget(['name' => 'subtaskContent', 'value' => $subtaskContent]);  ?>
-    </div>
-
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']) ?>
-    </div>
-    <?= Html::endForm(); ?>
-<?php else: ?>
-    <p>当前 OJ 运行模式不是 OI 模式，要启用子任务编辑，需要在后台设置页面启用 OI 模式。</p>
-<?php endif; ?>
+<div class="form-group">
+    <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-success btn-block']) ?>
+</div>
+<?= Html::endForm(); ?>
