d5d1466ef89333b670ae4c87c818948b76d4847c
Bobby Rong <rjl931189261@126.com>
2021-02-08 00:00:22 +0800

    sync layout for ranklist

--- modules/admin/views/contest/oi_rank.php
+++ modules/admin/views/contest/oi_rank.php
@@ -1,6 +1,8 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
+use app\models\Contest;
 
 /* @var $model app\models\Contest */
 
@@ -13,119 +15,131 @@ $submit_count = $rank_result['submit_count'];
 
 $this->registerAssetBundle('yii\bootstrap4\BootstrapPluginAsset');
 ?>
-<div class="wrap">
-    <div class="container">
-        <div class="alert alert-warning alert-dismissible fade in hidden-print" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-            <p>提示：</p>
-            <ul>
-                <li>可以使用浏览器自带的打印功能（Chrome 浏览器可在页面上鼠标“右键”-“打印”，其它浏览器请自行利用搜索引擎获取使用方法），
-                    可以选择将此页面导出为 PDF 格式。此提示信息不会出现在浏览器的打印窗口中。</li>
-                <li>比赛期间，若设了封榜，即使到了封榜期间，此榜单依然为实时榜单。</li>
-                <li>比赛结束后，由于前台依然能够开放提交，此榜单为比赛期间的榜单。</li>
-            </ul>
-        </div>
-        <div class="row">
-            <div class="col-md-3 text-left">
-                <strong>Start </strong>
-                <?= $model->start_time ?>
-            </div>
-            <div class="col-md-6 text-center">
-                <h2 class="contest-title"><?= Html::encode($model->title) ?></h2>
-            </div>
-            <div class="col-md-3 text-right">
-                <strong>End </strong>
-                <?= $model->end_time ?>
-            </div>
-        </div>
-        <table class="table table-bordered table-rank">
-            <thead>
-            <tr>
-                <th width="60px">Rank</th>
-                <th width="120px">Username</th>
-                <th width="120px">Nickname</th>
-                <th width="80px">测评总分</th>
-                <th width="80px">订正总分</th>
-                <th>
-                    用时
-                    <span data-toggle="tooltip" data-placement="top" title="仅统计所有通过题目所需要的总时间（单位：分钟）">
-                        <span class="glyphicon glyphicon-question-sign"></span>
-                    </span>
-                </th>
-                <?php foreach($problems as $key => $p): ?>
-                    <th>
-                        <?= chr(65 + $key) ?>
-                        <br>
-                        <span style="color:#7a7a7a; font-size:12px">
-                            <?php
-                            if (isset($submit_count[$p['problem_id']]['solved']))
-                                echo $submit_count[$p['problem_id']]['solved'];
-                            else
-                                echo 0;
-                            ?>
-                            /
-                            <?php
-                            if (isset($submit_count[$p['problem_id']]['submit']))
-                                echo $submit_count[$p['problem_id']]['submit'];
-                            else
-                                echo 0;
-                            ?>
-                        </span>
-                    </th>
-                <?php endforeach; ?>
-            </tr>
-            </thead>
-            <tbody>
-            <?php for ($i = 0, $ranking = 1; $i < count($result); $i++): ?>
-                <?php $rank = $result[$i]; ?>
-                <tr>
-                    <th>
+
+<p class="lead">查看比赛 <?= Html::encode($model->title) ?> 终榜。</p>
+<div class="text-center center-block">
+    <div class="table-responsive">
+
+
+        <table class="table table-bordered">
+
+            <tbody style="line-height: 1;">
+                <tr class="bg-tablehead" style="line-height: 2;">
+                    <td style="width:2.5rem"><b>#</b></td>
+                    <td style="width:8rem"></td>
+                    <td style="min-width:10rem;text-align:left"></td>
+                    <td style="width:3.5rem"><b>=</b></td>
+                    <?php foreach ($problems as $key => $p) : ?>
+                        <td>
+                            <b>
+                                <?= Html::a(chr(65 + $key), ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'text-dark']) ?>
+                            </b>
+                        </td>
+                    <?php endforeach; ?>
+                </tr>
+
+                <?php for ($i = 0, $ranking = 1, $last_ranking = 1; $i < count($result); $i++) : ?>
+                    <?php $rank = $result[$i]; ?>
+                    <tr>
+                        <td style="display:table-cell; vertical-align:middle">
+                            <?= $rank['finalrank']; ?>
+                        </td>
+                        <td style="display:table-cell; vertical-align:middle">
+                            <?= Html::encode($rank['student_number']); ?>
+
+                        </td>
+                        <td style="text-align:left;display:table-cell; vertical-align:middle">
+                            <?= Html::a(Html::encode($rank['nickname']), ['/user/view', 'id' => $rank['user_id']], ['class' => 'text-dark']) ?>
+                        </td>
+                        <td style="display:table-cell; vertical-align:middle">
+                            <span>
+                                <b>
+                                    <?php if ($model->type == Contest::TYPE_OI && $showStandingBeforeEnd == 1) : ?>
+                                        <?= $rank['total_score'] ?>
+                                    <?php else : ?>
+                                        <?= $rank['correction_score'] ?>
+                                    <?php endif ?>
+                                </b>
+                            </span>
+                        </td>
+
                         <?php
-                        //线下赛，参加比赛但不参加排名的处理
-                        if ($model->scenario == \app\models\Contest::SCENARIO_OFFLINE && $rank['role'] != \app\models\User::ROLE_PLAYER) {
-                            echo '*';
-                        } else {
-                            echo $ranking;
-                            $ranking++;
+                        foreach ($problems as $key => $p) {
+                            $score = '';
+                            $max_score = '';
+                            $css_class = ''; // CSS 颜色
+                            $first = ''; // 题目对应的排名表格第一行字的内容
+                            $second = ''; // 第二行字的内容
+                            if (isset($rank['solved_flag'][$p['problem_id']])) {
+                                $css_class = 'text-success font-weight-bold '; // 全部正确
+                            } else if ($model->type == Contest::TYPE_IOI) {
+                                if (isset($rank['max_score'][$p['problem_id']]) && $rank['max_score'][$p['problem_id']] > 0) {
+                                    $css_class = 'text-warning font-weight-bold '; // 部分正确
+                                } else if (isset($rank['max_score'][$p['problem_id']]) && $rank['max_score'][$p['problem_id']] == 0) {
+                                    $css_class = 'text-danger font-weight-bold '; // 尝试中
+                                }
+                            } else {
+                                if (isset($rank['score'][$p['problem_id']]) && $rank['score'][$p['problem_id']] > 0) {
+                                    $css_class = 'text-warning font-weight-bold '; // 部分正确
+                                } else if (isset($rank['score'][$p['problem_id']]) && $rank['score'][$p['problem_id']] == 0) {
+                                    $css_class = 'text-danger font-weight-bold '; // 尝试中
+                                }
+                            }
+
+                            if (isset($rank['score'][$p['problem_id']])) {
+                                $score = $rank['score'][$p['problem_id']];
+                                $max_score = $rank['max_score'][$p['problem_id']];
+                                if ($model->type == Contest::TYPE_OI && $showStandingBeforeEnd == 1) {
+                                    $first = $score;
+                                    $second = $max_score;
+                                    // IOI 模式下没必要记录最后一次得分，显示解答时间与得分
+                                } else {
+                                    $first = $max_score;
+                                    if (isset($rank['submit_time'][$p['problem_id']])) {
+                                        $min = intval($rank['submit_time'][$p['problem_id']]);
+                                        $second = sprintf("%02d:%02d", $min / 60, $min % 60);
+                                    }
+                                }
+                            }
+
+                            echo "<td style=\"display:table-cell; vertical-align:middle\"  class=\"{$css_class}\">{$first}</td>";
                         }
                         ?>
-                    </th>
-                    <th>
-                        <?= Html::encode($rank['username']); ?>
-                    </th>
-                    <th>
-                        <?= Html::encode($rank['nickname']); ?>
-                    </th>
-                    <th class="score-solved">
-                        <?= $rank['total_score'] ?>
-                    </th>
-                    <th class="score-time">
-                        <?= $rank['correction_score'] ?>
-                    </th>
-                    <th>
-                        <?= intval($rank['total_time']) ?>
-                    </th>
-                    <?php
-                    foreach($problems as $key => $p) {
-                        $score = "";
-                        $max_score = "";
-                        if (isset($rank['score'][$p['problem_id']])) {
-                            $score = $rank['score'][$p['problem_id']];
-                            $max_score = $rank['max_score'][$p['problem_id']];
-                        }
-                        echo "<th class=\"table-problem-cell\">{$score}<br><small>{$max_score}</small></th>";
-                    }
-                    ?>
+                    </tr>
+                <?php endfor; ?>
+                <tr class="bg-tablehead" style="line-height: 1;">
+                    <td style="width:2.5rem;display:table-cell; vertical-align:middle"><b>#</b></td>
+                    <td style="width:8rem"></td>
+                    <td style="min-width:10rem;text-align:left"></td>
+                    <td style="width:3.5rem;display:table-cell; vertical-align:middle"><b>=</b></td>
+                    <?php foreach ($problems as $key => $p) : ?>
+                        <td style="width:3.5rem">
+                            <span class="text-success">
+                                <b>
+                                    <?php
+                                    if (isset($submit_count[$p['problem_id']]['solved']))
+                                        echo $submit_count[$p['problem_id']]['solved'];
+                                    else
+                                        echo 0;
+                                    ?>
+                                </b>
+                            </span>
+                            <br>
+                            <span class="text-secondary">
+                                <b>
+                                    <?php
+                                    if (isset($submit_count[$p['problem_id']]['submit']))
+                                        echo $submit_count[$p['problem_id']]['submit'];
+                                    else
+                                        echo 0;
+                                    ?>
+                                </b>
+                            </span>
+                        </td>
+                    <?php endforeach; ?>
                 </tr>
-            <?php endfor; ?>
+
             </tbody>
         </table>
     </div>
 </div>
-
-<footer class="footer">
-    <div class="container">
-    <p class="pull-left"><center>&copy; SCNU SoCoding <?= date('Y') ?></center></p>
-        <!-- <p class="pull-left">&copy; <?= Yii::$app->setting->get('ojName') ?> OJ <?= date('Y') ?></p> -->
-    </div>
-</footer>
--- modules/admin/views/contest/rank.php
+++ modules/admin/views/contest/rank.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $model app\models\Contest */
 
@@ -14,135 +15,134 @@ $submit_count = $rank_result['submit_count'];
 $this->registerAssetBundle('yii\bootstrap4\BootstrapPluginAsset');
 ?>
 
-<div class="wrap">
-    <div class="container">
-        <div class="alert alert-warning alert-dismissible fade in hidden-print" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-            <p>提示：</p>
-            <ul>
-                <li>可以使用浏览器自带的打印功能（Chrome 浏览器可在页面上鼠标“右键”-“打印”，其它浏览器请自行利用搜索引擎获取使用方法），
-                    可以选择将此页面导出为 PDF 格式。此提示信息不会出现在浏览器的打印窗口中。</li>
-                <li>比赛期间，若设了封榜，即使到了封榜期间，此榜单依然为实时榜单。</li>
-                <li>比赛结束后，由于前台依然能够开放提交，此榜单为比赛期间的榜单。</li>
-            </ul>
-        </div>
-        <div class="row">
-            <div class="col-md-3 text-left">
-                <strong>Start </strong>
-                <?= $model->start_time ?>
-            </div>
-            <div class="col-md-6 text-center">
-                <h2 class="contest-title"><?= Html::encode($model->title) ?></h2>
-            </div>
-            <div class="col-md-3 text-right">
-                <strong>End </strong>
-                <?= $model->end_time ?>
-            </div>
-        </div>
-        <table class="table table-bordered table-rank">
-            <thead>
-            <tr>
-                <th width="60px">Rank</th>
-                <th width="80px">Username</th>
-                <th width="80px">Nickname</th>
-                <th width="80px">Number</th>
-                <th title="# solved / penalty time" colspan="2">Score</th>
-                <?php foreach($problems as $key => $p): ?>
-                    <th>
-                        <?= chr(65 + $key) ?>
-                        <br>
-                        <span style="color:#7a7a7a; font-size:12px">
+<p class="lead">查看比赛 <?= Html::encode($model->title) ?> 终榜。</p>
+<div class="text-center center-block">
+    <div class="table-responsive">
+        <table class="table table-bordered">
+
+            <tbody style="line-height: 1;">
+                <tr class="bg-tablehead" style="line-height: 2;">
+                    <td style="width:2.5rem"><b>#</b></td>
+                    <td style="width:8rem"></td>
+                    <td style="min-width:10rem;text-align:left"></td>
+                    <td style="width:3.5rem"><b>=</b></td>
+                    <?php foreach ($problems as $key => $p) : ?>
+                        <td style="width:3.5rem">
+                            <b>
+                                <?= Html::a(chr(65 + $key), ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'text-dark']) ?>
+                            </b>
+                        </td>
+                    <?php endforeach; ?>
+                </tr>
+                <?php for ($i = 0, $ranking = 1, $last_ranking = 1; $i < count($result); $i++) : ?>
+                    <?php $rank = $result[$i]; ?>
+                    <tr>
+                        <td style="display:table-cell; vertical-align:middle">
                             <?php
-                            if (isset($submit_count[$p['problem_id']]['solved']))
-                                echo $submit_count[$p['problem_id']]['solved'];
-                            else
-                                echo 0;
+                            echo $rank['finalrank'];
                             ?>
-                            /
-                            <?php
-                            if (isset($submit_count[$p['problem_id']]['submit']))
-                                echo $submit_count[$p['problem_id']]['submit'];
-                            else
-                                echo 0;
-                            ?>
-                        </span>
-                    </th>
-                <?php endforeach; ?>
-            </tr>
-            </thead>
-            <tbody>
-            <?php for ($i = 0, $ranking = 1; $i < count($result); $i++): ?>
-                <?php $rank = $result[$i]; ?>
-                <tr>
-                    <th>
+                        </td>
+                        <td style="display:table-cell; vertical-align:middle">
+                            <?= Html::encode($rank['student_number']); ?>
+                        </td>
+                        <td style="text-align:left;display:table-cell; vertical-align:middle">
+                            <?= Html::a(Html::encode($rank['nickname']), ['/user/view', 'id' => $rank['user_id']], ['class' => 'text-dark']) ?>
+                        </td>
+                        <td style="display:table-cell; vertical-align:middle">
+                            <span><b><?= $rank['solved'] ?></b></span>
+                            <span class="text-secondary">
+                                <?php if (strtotime($model->end_time) >= 253370736000) : ?>
+                                <?php else : ?>
+                                    <?php if (intval($rank['time'] / 60) < 100000) : ?>
+                                        <br><b><?= intval($rank['time'] / 60) ?></b>
+                                    <?php else : ?>
+                                        <br><b>10W+</b>
+                                    <?php endif; ?>
+                                <?php endif; ?>
+                            </span>
+                        </td>
+
                         <?php
-                        //线下赛，参加比赛但不参加排名的处理
-                        if ($model->scenario == \app\models\Contest::SCENARIO_OFFLINE && $rank['role'] != \app\models\User::ROLE_PLAYER) {
-                            echo '*';
-                        } else {
-                            echo $ranking;
-                            $ranking++;
+                        foreach ($problems as $key => $p) {
+                            $css_class = '';
+                            $num = '';
+                            $time = '';
+                            if (isset($rank['ac_time'][$p['problem_id']]) && $rank['ac_time'][$p['problem_id']] != -1) {
+                                if ($first_blood[$p['problem_id']] == $rank['user_id'] && strtotime($model->end_time) < 253370736000) {
+                                    $css_class = 'text-success bg-firstblood';
+                                } else {
+                                    $css_class = 'text-success';
+                                }
+                                if ($rank['wa_count'][$p['problem_id']] == 0) {
+                                    $num = '+';
+                                } else {
+                                    $num = '+' . $rank['wa_count'][$p['problem_id']];
+                                }
+                                if (intval($rank['ac_time'][$p['problem_id']]) < 100000) {
+                                    $time = '<br><span class="text-secondary">' . intval($rank['ac_time'][$p['problem_id']]) . '</span>';
+                                } else {
+                                    $time = '<br><span class="text-secondary">' . '10W+' . '</span>';
+                                }
+                                if (strtotime($model->end_time) >= 253370736000) {
+                                    $time = '';
+                                }
+
+                                if ($rank['ac_time'][$p['problem_id']] == 0) {
+                                    // 补题
+                                    $time = '';
+                                    $css_class = 'text-primary';
+                                }
+                            } else if (isset($rank['wa_count'][$p['problem_id']])) {
+                                $css_class = 'text-danger';
+                                if ($rank['wa_count'][$p['problem_id']] != 0) {
+                                    $num = '-' . $rank['wa_count'][$p['problem_id']];
+                                }
+                                $time = '';
+                            }
+                            // 封榜的显示
+                            if ($model->isScoreboardFrozen() && isset($rank['pending'][$p['problem_id']]) && $rank['pending'][$p['problem_id']]) {
+                                $num = "<span class=\"text-primary\">?<span>";
+                            }
+
+                            echo "<td style=\"display:table-cell; vertical-align:middle\"  class=\"{$css_class}\"><b>{$num}{$time}</b></td>";
                         }
                         ?>
-                    </th>
-                    <th>
-                        <?= Html::encode($rank['username']); ?>
-                    </th>
-                    <th>
-                        <?= Html::encode($rank['nickname']); ?>
-                    </th>
-                    <th>
-                        <?= Html::encode($rank['student_number']); ?>
-                    </th>
-                    <th class="score-solved">
-                        <?= $rank['solved'] ?>
-                    </th>
-                    <th class="score-time">
-                        <?= intval($rank['time'] / 60) ?>
-                    </th>
-                    <?php
-                    foreach($problems as $key => $p) {
-                        $css_class = "";
-                        $num = 0;
-                        $time = "";
-                        if (isset($rank['ac_time'][$p['problem_id']]) && $rank['ac_time'][$p['problem_id']] > 0) {
-                            if ($first_blood[$p['problem_id']] == $rank['user_id']) {
-                                $css_class = 'solved-first';
-                            } else {
-                                $css_class = 'solved';
-                            }
-                            $num = $rank['ce_count'][$p['problem_id']] + $rank['wa_count'][$p['problem_id']] + 1;
-                            $time = intval($rank['ac_time'][$p['problem_id']]);
-                        } else if (isset($rank['pending'][$p['problem_id']]) && $rank['pending'][$p['problem_id']]) {
-                            $css_class = 'pending';
-                            $num = $rank['ce_count'][$p['problem_id']] + $rank['wa_count'][$p['problem_id']] + $rank['pending'][$p['problem_id']];
-                            $time = '';
-                        } else if (isset($rank['wa_count'][$p['problem_id']])) {
-                            $css_class = 'attempted';
-                            $num = $rank['ce_count'][$p['problem_id']] + $rank['wa_count'][$p['problem_id']];
-                            $time = '';
-                        }
-                        if ($num == 0) {
-                            $num = '';
-                            $span = '';
-                        } else if ($num == 1) {
-                            $span = 'try';
-                        } else {
-                            $span = 'tries';
-                        }
-                        echo "<th class=\"table-problem-cell {$css_class}\">{$time}<br><small>{$num} {$span}</small></th>";
-                    }
-                    ?>
+                    </tr>
+                <?php endfor; ?>
+                <tr class="bg-tablehead" style="line-height: 1;">
+                    <td style="width:2.5rem;display:table-cell; vertical-align:middle"><b>#</b></td>
+                    <td style="width:8rem"></td>
+                    <td style="min-width:10rem;text-align:left"></td>
+                    <td style="width:3.5rem;display:table-cell; vertical-align:middle"><b>=</b></td>
+                    <?php foreach ($problems as $key => $p) : ?>
+                        <td style="width:3.5rem">
+                            <span class="text-success">
+                                <b>
+                                    <?php
+                                    if (isset($submit_count[$p['problem_id']]['solved']))
+                                        echo $submit_count[$p['problem_id']]['solved'];
+                                    else
+                                        echo 0;
+                                    ?>
+                                </b>
+                            </span>
+                            <br>
+                            <span class="text-secondary">
+                                <b>
+                                    <?php
+                                    if (isset($submit_count[$p['problem_id']]['submit']))
+                                        echo $submit_count[$p['problem_id']]['submit'];
+                                    else
+                                        echo 0;
+                                    ?>
+                                </b>
+                            </span>
+                        </td>
+                    <?php endforeach; ?>
                 </tr>
-            <?php endfor; ?>
+
+
             </tbody>
         </table>
     </div>
 </div>
-
-<footer class="footer">
-    <div class="container">
-    <p class="pull-left"><center>&copy; SCNU SoCoding <?= date('Y') ?></center></p>
-        <!-- <p class="pull-left">&copy; <?= Yii::$app->setting->get('ojName') ?> OJ <?= date('Y') ?></p> -->
-    </div>
-</footer>
