991017c972eeec04ce8ba8cf1ee50f693720d2c9
Bobby Rong <rjl931189261@126.com>
2021-03-11 11:32:07 +0800

    some tiny change related to group update

--- views/group/_form.php
+++ views/group/_form.php
@@ -17,22 +17,26 @@ use yii\widgets\ActiveForm;
         'template' => "<div class=\"input-group\"><div class=\"input-group-prepend\"><span class=\"input-group-text\">" . Yii::t('app', 'Names') . "</span></div>{input}</div>",
     ])->textInput(['maxlength' => true]) ?>
 
-    <div class="alert alert-light">小组描述所有人可见。</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 小组描述所有人可见。</div>
 
     <?= $form->field($model, 'description')->textarea(['maxlength' => true])->label(false) ?>
+
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 小组成员加入方式。</div>
 
     <?= $form->field($model, 'join_policy')->radioList([
         Group::JOIN_POLICY_INVITE => Yii::t('app', 'Invite Only'),
         Group::JOIN_POLICY_APPLICATION => Yii::t('app', 'Application & Approve'),
         Group::JOIN_POLICY_FREE => Yii::t('app', 'Free')
-    ]) ?>
+    ])->label(false) ?>
+
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 设置可见后用户可在探索页面发现这个小组。</div>
 
     <?= $form->field($model, 'status')->radioList([
         Group::STATUS_VISIBLE => Yii::t('app', 'Visible'),
         Group::STATUS_HIDDEN => Yii::t('app', 'Hidden')
-    ])->hint('可见：用户可在探索页面发现。') ?>
+    ])->label(false) ?>
 
-    <div class="alert alert-light">小组公告仅小组成员可见。</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 小组公告仅小组成员可见。</div>
 
     <?= $form->field($model, 'kanban', [
         'template' => "{input}",
