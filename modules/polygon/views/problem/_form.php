d9015df3f6077546f9de2fda5a2f1f072736dc9b
Bobby Rong <rjl931189261@126.com>
2021-03-11 10:51:56 +0800

    tiny change

--- modules/polygon/views/problem/_form.php
+++ modules/polygon/views/problem/_form.php
@@ -14,21 +14,21 @@ use yii\helpers\Url;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 题目标题</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 题目标题。</div>
     <?= $form->field($model, 'title', [
         'template' => "<div class=\"input-group\"><div class=\"input-group-prepend\"><span class=\"input-group-text\">标题</span></div>{input}</div>",
         'options' => ['class' => '']
     ])->textInput()->label(false) ?>
     <p></p>
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 单个测试点时间限制：Java 和 Python 有 2s 额外时间</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 单个测试点时间限制：Java 和 Python 有 2s 额外时间。</div>
     <?= $form->field($model, 'time_limit', [
         'template' => "<div class=\"input-group\"><div class=\"input-group-prepend\"><span class=\"input-group-text\">时间</span></div>{input}<div class=\"input-group-append\"><span class=\"input-group-text\">秒</span></div></div>",
         'options' => ['class' => '']
     ])->textInput(['maxlength' => 128, 'autocomplete' => 'off'])->label(false) ?>
     <p></p>
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 空间限制：Java 和 Python 有 128MB 额外空间</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 空间限制：Java 和 Python 有 128MB 额外空间。</div>
 
     <?= $form->field($model, 'memory_limit', [
         'template' => "<div class=\"input-group\"><div class=\"input-group-prepend\"><span class=\"input-group-text\">空间</span></div>{input}<div class=\"input-group-append\"><span class=\"input-group-text\">MB</span></div></div>",
@@ -36,19 +36,19 @@ use yii\helpers\Url;
     ])->textInput(['maxlength' => 128, 'autocomplete' => 'off'])->label(false) ?>
     <p></p>
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 题目描述</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 题目描述。</div>
 
     <?= $form->field($model, 'description')->widget('app\widgets\editormd\Editormd')->label(false); ?>
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 输入格式</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 输入格式。</div>
 
     <?= $form->field($model, 'input')->widget('app\widgets\editormd\Editormd')->label(false); ?>
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 输出格式</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 输出格式。</div>
 
     <?= $form->field($model, 'output')->widget('app\widgets\editormd\Editormd')->label(false); ?>
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 样例：请从样例 1 到样例 3 的顺序填写</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 样例：请从样例 1 到样例 3 的顺序填写。</div>
 
     <div class="row">
         <div class="col-md-6">
@@ -74,18 +74,18 @@ use yii\helpers\Url;
     </div>
 
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 是否开启 Special Judge：开启后需要你还需要在此处提交您的答案检查器</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 是否开启 Special Judge：开启后需要你还需要在此处提交您的答案检查器。</div>
 
     <?= $form->field($model, 'spj')->radioList([
         '1' => Yii::t('app', 'Yes'),
         '0' => Yii::t('app', 'No')
     ])->label(false) ?>
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 提示与说明</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 提示与说明。</div>
 
     <?= $form->field($model, 'hint')->widget('app\widgets\editormd\Editormd')->label(false); ?>
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 题目标签：多个标签使用逗号隔开，标签信息将展示在前台</div>
+    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 题目标签：多个标签使用逗号隔开，标签信息将展示在前台。</div>
 
     <?= $form->field($model, 'tags')->textarea(['maxlength' => true])->label(false) ?>
 
