b0b1f46053d89532594e5a0e8742282398fac943
Bobby Rong <rjl931189261@126.com>
2021-04-14 16:38:09 +0800

    redesign login page

--- views/site/login.php
+++ views/site/login.php
@@ -10,49 +10,53 @@ use yii\bootstrap4\ActiveForm;
 $this->title = Yii::t('app', 'Login');
 // $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
+<div class="site-login mt-5">
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
         'options' => [
             'class' => 'form-signin'
         ]
     ]); ?>
-        <h1><?= Html::encode($this->title) ?></h1>
-        <?= $form->field($model, 'username', [
-            'template' => '<div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-sm fa-fw fa-user"></i></span></div>{input}</div>{error}',
-            'inputOptions' => [
-                'placeholder' => $model->getAttributeLabel('username'),
-            ],
-        ])->label(false);
-        ?>
-        <?= $form->field($model, 'password', [
-           'template' => '<div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-sm fa-fw fa-lock"></i></span></div>{input}</div>{error}',
-           'inputOptions' => [
-                'placeholder' => $model->getAttributeLabel('password'),
-            ],
-        ])->passwordInput()->label(false);
-        ?>
+    <center>
+        <h2><?= Html::encode($this->title) ?></h2>
+    </center>
+    <br>
+    <?= $form->field($model, 'username', [
+        'template' => '<div class="input-group">{input}</div>{error}',
+        'inputOptions' => [
+            'placeholder' => $model->getAttributeLabel('username'),
+        ],
+    ])->label(false);
+    ?>
+    <?= $form->field($model, 'password', [
+        'template' => '<div class="input-group">{input}</div>{error}',
+        'inputOptions' => [
+            'placeholder' => $model->getAttributeLabel('password'),
+        ],
+    ])->passwordInput()->label(false);
+    ?>
 
-        <?php if ($model->scenario == 'withCaptcha'): ?>
-            <?= $form->field($model, 'verifyCode', [
-           'inputOptions' => [
+    <?php if ($model->scenario == 'withCaptcha') : ?>
+        <?= $form->field($model, 'verifyCode', [
+            'inputOptions' => [
                 'placeholder' => $model->getAttributeLabel('verifyCode'),
             ],
-        ])->widget(\yii\captcha\Captcha::class,[
+        ])->widget(\yii\captcha\Captcha::class, [
             'template' => '<div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-sm fa-fw fa-check"></i></span></div>{input}</div>
             <div class="list-group text-center" style="margin-top: 1rem;"><div class="list-group-item">{image}<span class="text-secondary d-none d-sm-inline">点击图片换图</span></div></div>',
         ])->label(false);
-    ?>
-        <?php endif; ?>
-        <?= $form->field($model, 'rememberMe')->checkbox() ?>
+        ?>
+    <?php endif; ?>
+    <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('app', 'Login'), ['class' => 'btn btn-block btn-primary btn-success', 'name' => 'login-button']) ?>
-            <div class="btn-group btn-block">
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Login'), ['class' => 'btn btn-block btn-primary btn-success', 'name' => 'login-button']) ?>
+        <div class="btn-group btn-block">
             <?= Html::a('忘记密码', ['site/request-password-reset'], ['class' => 'btn text-secondary']) ?>
             <?= Html::a('注册新帐号', ['site/signup'], ['class' => 'btn text-secondary']) ?>
         </div>
 
-    <?php ActiveForm::end(); ?>
+        <?php ActiveForm::end(); ?>
 
+    </div>
 </div>
--- views/site/signup.php
+++ views/site/signup.php
@@ -2,6 +2,7 @@
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap4\ActiveForm */
 /* @var $model app\models\SignupForm */
+
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
 
@@ -11,60 +12,61 @@ $this->title = Yii::t('app', 'Signup');
 
 
 
-<div class="form-signin">
-    <?php if (Yii::$app->setting->get('isUserReg')): ?>    
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="form-signin mt-5">
+    <?php if (Yii::$app->setting->get('isUserReg')) : ?>
+        <center>
+            <h2><?= Html::encode($this->title) ?></h2>
+        </center>
+        <br>
+        <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
-    <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+        <p class="text-secondary">用户名和学号一经设置不可修改。</p>
 
-    <div class="alert alert-light"><i class="fas fa-fw fa-info-circle"></i> 用户名和学号一经设置不可修改。</div>
-
-    <?= $form->field($model, 'username', [
-            'template' => '<div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-sm fa-fw fa-user"></i></span></div>{input}</div>{error}',
+        <?= $form->field($model, 'username', [
+            'template' => '<div class="input-group">{input}</div>{error}',
             'inputOptions' => [
                 'placeholder' => $model->getAttributeLabel('username'),
             ],
         ])->label(false);
-    ?>
-    <?= $form->field($model, 'email', [
-            'template' => '<div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-sm fa-fw fa-mail-bulk"></i></span></div>{input}</div>{error}',
+        ?>
+        <?= $form->field($model, 'email', [
+            'template' => '<div class="input-group">{input}</div>{error}',
             'inputOptions' => [
                 'placeholder' => $model->getAttributeLabel('email'),
             ],
         ])->label(false);
-    ?>
-    <?= $form->field($model, 'studentNumber', [
-            'template' => '<div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-sm fa-fw fa-info"></i></span></div>{input}</div>{error}',
+        ?>
+        <?= $form->field($model, 'studentNumber', [
+            'template' => '<div class="input-group">{input}</div>{error}',
             'inputOptions' => [
                 'placeholder' => $model->getAttributeLabel('studentNumber'),
             ],
         ])->label(false);
-    ?>
-    <?= $form->field($model, 'password', [
-           'template' => '<div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-sm fa-fw fa-lock"></i></span></div>{input}</div>{error}',
-           'inputOptions' => [
+        ?>
+        <?= $form->field($model, 'password', [
+            'template' => '<div class="input-group">{input}</div>{error}',
+            'inputOptions' => [
                 'placeholder' => $model->getAttributeLabel('password'),
             ],
         ])->passwordInput()->label(false);
-    ?>
+        ?>
 
-    <?= $form->field($model, 'verifyCode', [
-           'inputOptions' => [
+        <?= $form->field($model, 'verifyCode', [
+            'inputOptions' => [
                 'placeholder' => $model->getAttributeLabel('verifyCode'),
             ],
-        ])->widget(\yii\captcha\Captcha::class,[
-            'template' => '<div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-sm fa-fw fa-check"></i></span></div>{input}</div>
+        ])->widget(\yii\captcha\Captcha::class, [
+            'template' => '<div class="input-group">{input}</div>
             <div class="list-group text-center" style="margin-top: 1rem;"><div class="list-group-item">{image}<a href="signup"><span class="text-secondary d-none d-sm-inline">点击图片换图</span></a></div></div>',
         ])->label(false);
-    ?>
+        ?>
 
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Signup'), ['class' => 'btn btn-success btn-block', 'name' => 'signup-button']) ?>
-        <?= Html::a('已有帐号', ['site/login'], ['class' => 'btn btn-block text-secondary']) ?>
-    </div>
-    <?php ActiveForm::end(); ?>
-    <?php else: ?>
-       <h3> 当前未开放注册！</h3>
-    <?php endif; ?>    
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'Signup'), ['class' => 'btn btn-success btn-block', 'name' => 'signup-button']) ?>
+            <?= Html::a('已有帐号', ['site/login'], ['class' => 'btn btn-block text-secondary']) ?>
+        </div>
+        <?php ActiveForm::end(); ?>
+    <?php else : ?>
+        <h3> 当前未开放注册！</h3>
+    <?php endif; ?>
 </div>
-
