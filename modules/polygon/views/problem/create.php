6fdd0e9ebb48766d7cc9d23b24f905cabd67a3e4
Bobby Rong <rjl931189261@126.com>
2021-05-08 17:09:41 +0800

    add some notes in polygon problem update page

--- modules/polygon/views/problem/create.php
+++ modules/polygon/views/problem/create.php
@@ -15,7 +15,7 @@ $this->title = Yii::t('app', 'Create Problem');
 
     <p class="lead">创建一道题目。</p>
 
-    <div class="alert alert-light">
+    <div class="alert alert-info">
         <i class="fas fa-fw fa-info-circle"></i> 欢迎使用 Polygon 系统，在此处上传题目即代表你同意管理员同步你的题目到公共题库供其他用户提交。
     </div>
 
--- modules/polygon/views/problem/update.php
+++ modules/polygon/views/problem/update.php
@@ -13,6 +13,10 @@ $this->params['model'] = $model;
 ?>
 <div class="problem-update">
 
+    <div class="alert alert-info">
+        <i class="fas fa-fw fa-info-circle"></i> 如果你对题面进行了修改并希望管理员同步到主题库，请在题目标题一栏标明以方便管理员及时处理。
+    </div>
+
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
