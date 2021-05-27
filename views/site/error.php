f439e5f6cb578f0e61c72e091a3b5a31369d1bda
Bobby Rong <rjl931189261@126.com>
2021-05-27 20:49:44 +0800

    animate error page

--- views/site/error.php
+++ views/site/error.php
@@ -15,11 +15,11 @@ $this->title = $name;
     <i class="fas fa-fw fa-info-circle"></i> <?= nl2br(Html::encode($message)) ?>
 </div>
 
-<div class="card">
-    <img src="<?= Yii::getAlias('@web') . '/images/error-header.jpg' ?>" class="card-img-top d-none d-md-block">
+<div class="card animate__animated animate__fadeIn animate__faster">
+    <img src="<?= Yii::getAlias('@web') . '/images/error-header.png' ?>" class="card-img-top d-none d-md-block">
     <div class="card-body">
         <h3 class="card-title"><?= Html::encode($this->title) ?></h3>
-        非常抱歉，页面没有如您所期望地加载，在大多数情况下可能是因为访问权限受限或者页面不存在。<br>
+        页面没有如您所期望地加载，在大多数情况下可能是因为访问权限受限或者页面不存在。<br>
         如果您认为这是站点本身的问题，欢迎您联系 <a target="_blank" href="https://github.com/scnu-socoding/scnuoj">SCNU Online Judge 开发组</a>。
         <p></p>
         <a class="btn-block btn btn-primary" href="<?=Yii::$app->homeUrl?>"> <i class="fas fa-fw fa-home"></i> 返回首页</a>
