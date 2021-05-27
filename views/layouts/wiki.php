37618a6793e8afa09a82e58e34c2882117e58b1d
Bobby Rong <rjl931189261@126.com>
2021-05-27 20:49:32 +0800

    some code format

--- views/layouts/wiki.php
+++ views/layouts/wiki.php
@@ -11,33 +11,33 @@ $this->title = Yii::t('app', 'Wiki');;
 <?php $this->beginContent('@app/views/layouts/main.php'); ?>
 
 <?= Nav::widget([
-            'items' => [
-                [
-                    'label' => Yii::t('app', '判题说明'), 'url' => ['wiki/index']
-                ],
-                [
-                    'label' => Yii::t('app', '常见问题'), 'url' => ['wiki/faq']
-                ],
-                ['label' => Yii::t('app', 'About'), 'url' => ['wiki/about']],
-                [
-                    'label' => Yii::t('app', 'Contest'), 'url' => ['wiki/contest'],
-                    'visible' => !Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin()
-                ],
-                [
-                    'label' => Yii::t('app', '出题要求'), 'url' => ['wiki/problem'],
-                    'visible' => !Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin()
-                ],
-                [
-                    'label' => Yii::t('app', 'Special Judge'), 'url' => ['wiki/spj'],
-                    'visible' => !Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin()
-                ],
-                [
-                    'label' => Yii::t('app', 'OI 模式'), 'url' => ['wiki/oi'],
-                    'visible' => !Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin()
-                ]
-            ],
-            'options' => ['class' => 'nav nav-pills']
-        ]) ?>
+    'items' => [
+        [
+            'label' => Yii::t('app', '判题说明'), 'url' => ['wiki/index']
+        ],
+        [
+            'label' => Yii::t('app', '常见问题'), 'url' => ['wiki/faq']
+        ],
+        ['label' => Yii::t('app', 'About'), 'url' => ['wiki/about']],
+        [
+            'label' => Yii::t('app', 'Contest'), 'url' => ['wiki/contest'],
+            'visible' => !Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin()
+        ],
+        [
+            'label' => Yii::t('app', '出题要求'), 'url' => ['wiki/problem'],
+            'visible' => !Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin()
+        ],
+        [
+            'label' => Yii::t('app', 'Special Judge'), 'url' => ['wiki/spj'],
+            'visible' => !Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin()
+        ],
+        [
+            'label' => Yii::t('app', 'OI 模式'), 'url' => ['wiki/oi'],
+            'visible' => !Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin()
+        ]
+    ],
+    'options' => ['class' => 'nav nav-pills']
+]) ?>
 <p></p>
 <div class="row">
     <div class="col animate__animated animate__fadeIn animate__faster">
