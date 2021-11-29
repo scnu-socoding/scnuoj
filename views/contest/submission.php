f8bf6e816040c4044cbd4e8161ea942ef263aff7
Bobby Rong <rjl931189261@126.com>
2021-11-29 20:32:23 +0800

    update styling for submission list

--- views/contest/submission.php
+++ views/contest/submission.php
@@ -6,18 +6,38 @@ use app\models\Solution;
 /* @var $this yii\web\View */
 /* @var $submissions array */
 ?>
-<?php foreach ($submissions as $submission): ?>
-    <p>
-        <span class="glyphicon glyphicon-time"></span> <?= $submission['created_at'] ?>
-        <?php
-        $label = Solution::getResultList($submission['result']);
-        if ($submission['result'] == Solution::OJ_AC) {
-            echo "<span class=\"badge badge-success\">{$label}</span>";
-        } else {
-            echo "<span class=\"badge badge-danger\">{$label}</span>";
-        }
-        ?>
-        →
-        <?= Html::a($submission['id'], ['/solution/detail', 'id' => $submission['id']], ['target' => '_blank']) ?>
-    </p>
-<?php endforeach; ?>
+<p class="lead" style="text-align: left;">查看提交历史</p>
+<?php if (empty($submissions)) : ?>
+    <div class="list-group">
+        <div class="list-group-item" style="text-align: left;">
+            没有找到数据。
+        </div>
+    </div>
+<?php else : ?>
+    <div class="list-group">
+        <?php foreach ($submissions as $submission) : ?>
+
+            <?php
+            if ($submission['result'] == Solution::OJ_AC)
+                $label_color = "text-success";
+            else if ($submission['result'] == Solution::OJ_WA)
+                $label_color = "text-danger";
+            else
+                $label_color = "text-warning";
+            ?>
+
+            <?= Html::a(
+                '<span class="' . $label_color . '">' . Solution::getResultList($submission['result']) . '</span>' .
+                    '<span class="float-right text-secondary">' . $submission['created_at'] . '</span>',
+                [
+                    '/solution/detail', 'id' => $submission['id']
+                ],
+                [
+                    'class' => 'list-group-item list-group-item-action',
+                    'style' => 'text-align: left;',
+                    'target' => '_blank'
+                ]
+            ) ?>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
