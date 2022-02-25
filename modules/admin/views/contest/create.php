ec522208ce8bdc94fc492854ef15879e6837a547
Bobby Rong <rjl931189261@126.com>
2022-02-25 12:53:49 +0800

    add default value for enable_board

--- modules/admin/views/contest/create.php
+++ modules/admin/views/contest/create.php
@@ -14,6 +14,7 @@ $model->status = $model::STATUS_HIDDEN;
 $model->type = $model::TYPE_RANK_GROUP;
 $model->scenario = $model::SCENARIO_ONLINE;
 $model->enable_clarify = 1;
+$model->enable_board = 1;
 $model->enable_print = 0;
 $model->punish_time = 20; //罚时初始值
 ?>
