600acdb15aa45b07670e6aed79730605dc8b5277
Bobby Rong <rjl931189261@126.com>
2022-02-26 22:43:00 +0800

    layouts/basic: drop unused use

--- modules/admin/views/layouts/basic.php
+++ modules/admin/views/layouts/basic.php
@@ -3,11 +3,7 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use app\widgets\Alert;
 use yii\helpers\Html;
-use yii\bootstrap4\Nav;
-use yii\bootstrap4\NavBar;
-use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($this);
