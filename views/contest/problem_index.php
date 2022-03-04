3be66b1dd329e64a40a2cd0e1440a7088e6b02d2
Bobby Rong <rjl931189261@126.com>
2022-03-04 20:02:45 +0800

    problem_index: simplify code

--- views/contest/problem_index.php
+++ views/contest/problem_index.php
@@ -49,15 +49,18 @@ $submissionStatistics = $model->getSubmissionStatistics();
                         : chr(65 + $key);
                     ?>
 
-                    <?php if (!isset($loginUserProblemSolvingStatus[$p['problem_id']])) : ?>
-                        <?= Html::a('<b>Problem ' . $problem_id . '.</b> ' . Html::encode($p['title']) . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['solved'] . ' 通过 / ' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'list-group-item list-group-item-action']) ?>
-                    <?php elseif ($loginUserProblemSolvingStatus[$p['problem_id']] == \app\models\Solution::OJ_AC) : ?>
-                        <?= Html::a('<b>Problem ' . $problem_id . '.</b> ' . Html::encode($p['title']) . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['solved'] . ' 通过 / ' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'list-group-item list-group-item-action list-group-item-success']) ?>
-                    <?php elseif ($loginUserProblemSolvingStatus[$p['problem_id']] < 4) : ?>
-                        <?= Html::a('<b>Problem ' . $problem_id . '.</b> ' . Html::encode($p['title']) . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['solved'] . ' 通过 / ' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'list-group-item list-group-item-action list-group-item-warning']) ?>
-                    <?php else : ?>
-                        <?= Html::a('<b>Problem ' . $problem_id . '.</b> ' . Html::encode($p['title']) . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['solved'] . ' 通过 / ' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => 'list-group-item list-group-item-action list-group-item-danger']) ?>
-                    <?php endif; ?>
+                    <?php
+                    $problem_class = "list-group-item list-group-item-action ";
+                    if (!isset($loginUserProblemSolvingStatus[$p['problem_id']])) {
+                    } elseif ($loginUserProblemSolvingStatus[$p['problem_id']] == \app\models\Solution::OJ_AC) {
+                        $problem_class .= "list-group-item-success";
+                    } elseif ($loginUserProblemSolvingStatus[$p['problem_id']] < 4) {
+                        $problem_class .= "list-group-item-warning";
+                    } else {
+                        $problem_class .= "list-group-item-danger";
+                    }
+                    ?>
+                    <?= Html::a('<b>Problem ' . $problem_id . '.</b> ' . Html::encode($p['title']) . '<span class="float-right">' . $submissionStatistics[$p['problem_id']]['solved'] . ' 通过 / ' . $submissionStatistics[$p['problem_id']]['submit'] . ' 提交</span>', ['/contest/problem', 'id' => $model->id, 'pid' => $key], ['class' => $problem_class]) ?>
                 <?php endforeach; ?>
             <?php endif; ?>
         </div>
